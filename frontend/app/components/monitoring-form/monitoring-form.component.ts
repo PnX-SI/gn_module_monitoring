98b7f5be13833f3bc9f9ee9390510d79d3282dd4
joel.clement <joelclems@gmail.com>
2020-01-23 11:50:29 +0100

    validator taxon

--- frontend/app/components/monitoring-form/monitoring-form.component.ts
+++ frontend/app/components/monitoring-form/monitoring-form.component.ts
@@ -1,16 +1,16 @@
-import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-import { MonitoringObject } from "../../class/monitoring-object";
-import { Utils } from "../../utils/utils";
-import { Router } from "@angular/router";
-import { ConfigService } from "../../services/config.service";
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { MonitoringObject } from '../../class/monitoring-object';
+import { Utils } from '../../utils/utils';
+import { Router } from '@angular/router';
+import { ConfigService } from '../../services/config.service';
 
-import { mergeMap } from "@librairies/rxjs/operators";
+import { mergeMap } from '@librairies/rxjs/operators';
 
 @Component({
-  selector: "pnx-monitoring-form",
-  templateUrl: "./monitoring-form.component.html",
-  styleUrls: ["./monitoring-form.component.css"]
+  selector: 'pnx-monitoring-form',
+  templateUrl: './monitoring-form.component.html',
+  styleUrls: ['./monitoring-form.component.css']
 })
 export class MonitoringFormComponent implements OnInit {
   @Input() currentUser;
@@ -46,7 +46,7 @@ export class MonitoringFormComponent implements OnInit {
       .pipe(
         mergeMap(() => {
           this.bChainInput = this._configService.frontendParams()[
-            "bChainInput"
+            'bChainInput'
           ];
           this.objSchema = this.obj.schema().filter(e => e.type_widget);
           return this.obj.formValues();
@@ -54,10 +54,10 @@ export class MonitoringFormComponent implements OnInit {
       )
       .subscribe(formValues => {
         // set geometry
-        if (this.obj.config["geometry_type"]) {
+        if (this.obj.config['geometry_type']) {
           this.objForm.addControl(
-            "geometry",
-            this._formBuilder.control("", Validators.required)
+            'geometry',
+            this._formBuilder.control('', Validators.required)
           );
         }
         this.setFormValue(formValues);
@@ -66,7 +66,7 @@ export class MonitoringFormComponent implements OnInit {
 
   isFormReady() {
     let schemaFormSize = this.objSchema.filter(elem => elem.type_widget).length;
-    if (this.obj.config["geometry_type"]) {
+    if (this.obj.config['geometry_type']) {
       schemaFormSize += 1;
     }
     const formSize = Utils.dictSize(this.objForm.controls);
@@ -95,7 +95,7 @@ export class MonitoringFormComponent implements OnInit {
       null,
       this.obj.monitoringObjectService()
     );
-    this.obj.properties[this.obj.configParam("id_field_Name")] = null;
+    this.obj.properties[this.obj.configParam('id_field_Name')] = null;
     this.obj.parentId = parentId;
     this.obj
       .get(0)
@@ -115,27 +115,27 @@ export class MonitoringFormComponent implements OnInit {
     const values = this.objForm.value;
     const defaultValues = {};
 
-    defaultValues["id_digitiser"] =
-      values["id_digitiser"] || this.currentUser.id_role;
+    defaultValues['id_digitiser'] =
+      values['id_digitiser'] || this.currentUser.id_role;
     this.objForm.patchValue(defaultValues);
   }
 
   navigateToParent() {
     this.bEditChange.emit(false);
-    if (this.obj.objectType.includes("module")) {
+    if (this.obj.objectType.includes('module')) {
       this._router.navigate([
-        "/",
+        '/',
         this._configService.frontendModuleMonitoringUrl(),
-        "object",
+        'object',
         this.obj.modulePath,
-        "module",
+        'module',
         this.obj.id
       ]);
     } else {
       this._router.navigate([
-        "/",
+        '/',
         this._configService.frontendModuleMonitoringUrl(),
-        "object",
+        'object',
         this.obj.modulePath,
         this.obj.parentType(),
         this.obj.parentId
@@ -144,12 +144,12 @@ export class MonitoringFormComponent implements OnInit {
   }
 
   reload_create_route() {
-    this._router.navigate(["/"]);
+    this._router.navigate(['/']);
     setTimeout(() => {
       this._router.navigate([
-        "/",
+        '/',
         this._configService.frontendModuleMonitoringUrl(),
-        "create_object",
+        'create_object',
         this.obj.modulePath,
         this.obj.objectType,
         this.obj.parentId
@@ -164,11 +164,11 @@ export class MonitoringFormComponent implements OnInit {
     const action = this.obj.id
       ? this.obj.patch(this.objForm.value)
       : this.obj.post(this.objForm.value);
-    const actionLabel = this.obj.id ? "Modification" : "Création";
+    const actionLabel = this.obj.id ? 'Modification' : 'Création';
     action.subscribe(objData => {
       console.log(
-        "info",
-        `${actionLabel} de ${this.obj.configParam("label")} ${
+        'info',
+        `${actionLabel} de ${this.obj.configParam('label')} ${
           this.obj.id
         } effectué`
       );
@@ -193,20 +193,20 @@ export class MonitoringFormComponent implements OnInit {
 
   onDelete() {
     this.bDeleteSpinner = true;
-    const msg_delete = `${this.obj.template["label"]} ${
+    const msg_delete = `${this.obj.template['label']} ${
       this.obj.id
     } supprimé. parent ${this.obj.parentType()} ${this.obj.parentId}`;
-    console.log(msg_delete)
-    
+    console.log(msg_delete);
+
     this.obj.delete().subscribe(objData => {
       this.bDeleteSpinner = this.bDeleteModal = false;
       this.objChanged.emit(this.obj);
       // this.bEditChange.emit(false);
-      this.navigateToParent();  
+      this.navigateToParent();
     });
   }
 
   bChainInputChanged() {
-    this._configService.setFrontendParams("bChainInput", this.bChainInput);
+    this._configService.setFrontendParams('bChainInput', this.bChainInput);
   }
 }
