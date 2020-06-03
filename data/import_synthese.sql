e54dbcda8c8646528510740e41839aa26ca4fe59
amandine.sahl <amandine.sahl@gmail.com>
2020-06-03 17:57:48 +0200

    Bug ids_observers null + serialize geom from geojson

--- data/import_synthese.sql
+++ data/import_synthese.sql
@@ -1,9 +1,8 @@
-
-
--- Fonction import dans la synthese
+-- Fonctions import dans la synthese
 
 CREATE OR REPLACE FUNCTION gn_synthese.import_json_row(
-	datain json
+	datain jsonb,
+  datageojson text default NULL -- données géographique sous la forme d'un geojson TODO a voir le paramètre du type text ou json
 )
 RETURNS boolean AS
 $BODY$
@@ -12,97 +11,119 @@ $BODY$
     select_columns text;
     update_columns text;
 
-  BEGIN
+    geom geometry;
+    geom_data jsonb;
+    local_srid int;
+BEGIN
 
-    -- Import des données dans une table temporaire pour faciliter le traitement
-	DROP TABLE IF EXISTS tmp_process_import;
-    CREATE TEMP TABLE tmp_process_import (
-        id_synthese int,
-        datain json,
-        action char(1)
+
+  -- Import des données dans une table temporaire pour faciliter le traitement
+  DROP TABLE IF EXISTS tmp_process_import;
+  CREATE TABLE tmp_process_import (
+      id_synthese int,
+      datain jsonb,
+      action char(1)
+  );
+  INSERT INTO tmp_process_import (datain)
+  SELECT datain;
+
+  -- Cas ou la geométrie est passé en geojson
+  IF NOT datageojson IS NULL THEN
+    geom := (SELECT ST_setsrid(ST_GeomFromGeoJSON(datageojson), 4326));
+    local_srid := (SELECT parameter_value FROM gn_commons.t_parameters WHERE parameter_name = 'local_srid');
+    geom_data := (
+        SELECT json_build_object(
+            'the_geom_4326',geom,
+            'the_geom_point',(SELECT ST_centroid(geom)),
+            'the_geom_local',(SELECT ST_transform(geom, local_srid))
+        )
     );
-    INSERT INTO tmp_process_import (datain)
-    SELECT datain;
+
+    UPDATE tmp_process_import d
+      SET datain = d.datain || geom_data;
+  END IF;
 
 -- ############ TEST
 
-	-- colonne unique_id_sinp exists
-	IF EXISTS (
-        SELECT 1 FROM json_object_keys(datain) column_name WHERE column_name =  'unique_id_sinp'
+  -- colonne unique_id_sinp exists
+  IF EXISTS (
+        SELECT 1 FROM jsonb_object_keys(datain) column_name WHERE column_name =  'unique_id_sinp'
     ) IS FALSE THEN
         RAISE NOTICE 'Column unique_id_sinp is mandatory';
         RETURN FALSE;
-	END IF ;
+  END IF ;
 
 -- ############ mapping colonnes
 
-    WITH import_col AS (
-		SELECT json_object_keys(datain) AS column_name
-	), synt_col AS (
-        SELECT column_name, column_default, CASE WHEN data_type = 'USER-DEFINED' THEN NULL ELSE data_type END as data_type
-        FROM information_schema.columns
-        WHERE table_schema || '.' || table_name = 'gn_synthese.synthese'
-    )
-    SELECT
-        string_agg(s.column_name, ',')  as insert_columns,
-        string_agg(
-            CASE
-                WHEN NOT column_default IS NULL THEN 'COALESCE((datain->>''' || i.column_name  || ''')' || COALESCE('::' || data_type, '') || ', ' || column_default || ') as ' || i.column_name
-            ELSE '(datain->>''' || i.column_name  || ''')' || COALESCE('::' || data_type, '')
-            END, ','
-        ) as select_columns ,
-        string_agg(
-            s.column_name || '=' || CASE
-                WHEN NOT column_default IS NULL THEN 'COALESCE((datain->>''' || i.column_name  || ''')' || COALESCE('::' || data_type, '') || ', ' || column_default || ') '
-            ELSE '(datain->>''' || i.column_name  || ''')' || COALESCE('::' || data_type, '')
-            END
-        , ',')
-    INTO insert_columns, select_columns, update_columns
-    FROM synt_col s
-    JOIN import_col i
-    ON i.column_name = s.column_name;
+  WITH import_col AS (
+    SELECT jsonb_object_keys(datain) AS column_name
+  ), synt_col AS (
+      SELECT column_name, column_default, CASE WHEN data_type = 'USER-DEFINED' THEN NULL ELSE data_type END as data_type
+      FROM information_schema.columns
+      WHERE table_schema || '.' || table_name = 'gn_synthese.synthese'
+  )
+  SELECT
+      string_agg(s.column_name, ',')  as insert_columns,
+      string_agg(
+          CASE
+              WHEN NOT column_default IS NULL THEN 'COALESCE((datain->>''' || i.column_name  || ''')' || COALESCE('::' || data_type, '') || ', ' || column_default || ') as ' || i.column_name
+          ELSE '(datain->>''' || i.column_name  || ''')' || COALESCE('::' || data_type, '')
+          END, ','
+      ) as select_columns ,
+      string_agg(
+          s.column_name || '=' || CASE
+              WHEN NOT column_default IS NULL THEN 'COALESCE((datain->>''' || i.column_name  || ''')' || COALESCE('::' || data_type, '') || ', ' || column_default || ') '
+          ELSE '(datain->>''' || i.column_name  || ''')' || COALESCE('::' || data_type, '')
+          END
+      , ',')
+  INTO insert_columns, select_columns, update_columns
+  FROM synt_col s
+  JOIN import_col i
+  ON i.column_name = s.column_name;
 
-    -- ############# IMPORT DATA
-   	IF EXISTS (
-        SELECT 1
-        FROM   gn_synthese.synthese
-        WHERE  unique_id_sinp = (datain->>'unique_id_sinp')::uuid
-    ) IS TRUE THEN
-		-- Update
-		EXECUTE ' WITH i_row AS (
-            UPDATE gn_synthese.synthese s SET ' || update_columns ||
-            ' FROM  tmp_process_import
-            WHERE s.unique_id_sinp =  (datain->>''unique_id_sinp'')::uuid
-            RETURNING s.id_synthese, s.unique_id_sinp
-            )
-            UPDATE tmp_process_import d SET id_synthese = i_row.id_synthese
-            FROM i_row
-            WHERE unique_id_sinp = i_row.unique_id_sinp
-            ' ;
-    ELSE
-		-- Insert
-       EXECUTE 'WITH i_row AS (
-            INSERT INTO gn_synthese.synthese ( ' || insert_columns || ')
-            SELECT ' || select_columns ||
-            ' FROM tmp_process_import
-            RETURNING id_synthese, unique_id_sinp
-            )
-            UPDATE tmp_process_import d SET id_synthese = i_row.id_synthese
-            FROM i_row
-            WHERE unique_id_sinp = i_row.unique_id_sinp
-            ' ;
-    END IF;
+  -- ############# IMPORT DATA
+  IF EXISTS (
+      SELECT 1
+      FROM   gn_synthese.synthese
+      WHERE  unique_id_sinp = (datain->>'unique_id_sinp')::uuid
+  ) IS TRUE THEN
+    -- Update
+    EXECUTE ' WITH i_row AS (
+          UPDATE gn_synthese.synthese s SET ' || update_columns ||
+          ' FROM  tmp_process_import
+          WHERE s.unique_id_sinp =  (datain->>''unique_id_sinp'')::uuid
+          RETURNING s.id_synthese, s.unique_id_sinp
+          )
+          UPDATE tmp_process_import d SET id_synthese = i_row.id_synthese
+          FROM i_row
+          WHERE unique_id_sinp = i_row.unique_id_sinp
+          ' ;
+  ELSE
+    -- Insert
+    EXECUTE 'WITH i_row AS (
+          INSERT INTO gn_synthese.synthese ( ' || insert_columns || ')
+          SELECT ' || select_columns ||
+          ' FROM tmp_process_import
+          RETURNING id_synthese, unique_id_sinp
+          )
+          UPDATE tmp_process_import d SET id_synthese = i_row.id_synthese
+          FROM i_row
+          WHERE unique_id_sinp = i_row.unique_id_sinp
+          ' ;
+  END IF;
 
-	-- Import des cor_observers
-    DELETE FROM gn_synthese.cor_observer_synthese
-    USING tmp_process_import
-	WHERE cor_observer_synthese.id_synthese = tmp_process_import.id_synthese;
+  -- Import des cor_observers
+  DELETE FROM gn_synthese.cor_observer_synthese
+  USING tmp_process_import
+  WHERE cor_observer_synthese.id_synthese = tmp_process_import.id_synthese;
 
+  IF jsonb_typeof(datain->'ids_observers') = 'array' THEN
     INSERT INTO gn_synthese.cor_observer_synthese (id_synthese, id_role)
-    SELECT DISTINCT id_synthese, (json_array_elements(t.datain->'ids_observers'))::text::int
+    SELECT DISTINCT id_synthese, (jsonb_array_elements(t.datain->'ids_observers'))::text::int
     FROM tmp_process_import t;
+  END IF;
 
-	RETURN TRUE;
+  RETURN TRUE;
   END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
@@ -112,8 +133,8 @@ $BODY$
 
 CREATE OR REPLACE FUNCTION gn_synthese.import_row_from_table(
   select_col_name varchar,
-	select_col_val varchar,
-	table_name varchar
+  select_col_val varchar,
+  table_name varchar
 )
 RETURNS boolean AS
 $BODY$
@@ -122,7 +143,7 @@ $BODY$
     import_rec record;
   BEGIN
     -- TODO transtypage en text pour des questions de généricité. A réflechir
-    select_sql := 'SELECT row_to_json(c) d
+    select_sql := 'SELECT row_to_json(c)::jsonb d
         FROM ' || table_name || ' c
         WHERE ' ||  select_col_name|| '::text = ''' || select_col_val || '''' ;
 
@@ -130,7 +151,7 @@ $BODY$
         PERFORM gn_synthese.import_json_row(import_rec.d);
     END LOOP;
 
-	RETURN TRUE;
+  RETURN TRUE;
   END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
