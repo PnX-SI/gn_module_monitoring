87f2491245d477e97f428b657be9249b086b68a1
amandine.sahl <amandine.sahl@gmail.com>
2020-06-03 15:18:40 +0200

    Trigger de suppression des données dans la synthèse

---
+++ data/delete_synthese.sql
@@ -0,0 +1,28 @@
+--  ###  Triggers permettant d'assurer la suppression dans la synthèse
+
+-- Visites
+
+CREATE FUNCTION gn_synthese.fct_trg_delete_synthese_visits() RETURNS trigger AS $$
+BEGIN
+    --Suppression des données dans la synthèse
+    DELETE FROM gn_synthese.synthese WHERE unique_id_sinp = OLD.uuid_base_visit;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_delete_synthese_visits AFTER DELETE ON gn_monitoring.t_base_visits
+    FOR EACH ROW EXECUTE PROCEDURE gn_synthese.fct_trg_delete_synthese_visits();
+
+
+-- Observations
+CREATE FUNCTION gn_synthese.fct_trg_delete_synthese_observations() RETURNS trigger AS $$
+BEGIN
+    --Suppression des données dans la synthèse
+    DELETE FROM gn_synthese.synthese WHERE unique_id_sinp = OLD.uuid_observation;
+    RETURN OLD;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER trg_delete_synthese_observations AFTER DELETE ON gn_monitoring.t_observations
+    FOR EACH ROW EXECUTE PROCEDURE gn_synthese.fct_trg_delete_synthese_observations();
+
