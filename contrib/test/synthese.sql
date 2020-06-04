95de77f4d9e575c18e0d5230da97708bfc86b9fc
joel.clement <joelclems@gmail.com>
2020-06-04 17:22:01 +0200

    entity_source_pk_value test cf #14

--- contrib/test/synthese.sql
+++ contrib/test/synthese.sql
@@ -37,7 +37,7 @@ SELECT
 		o.uuid_observation AS unique_id_sinp, 
 		v.uuid_base_visit AS unique_id_sinp_grp,
 		(SELECT id_source FROM source) as id_source,
-		v.id_observation AS entity_source_pk_value,
+		o.id_observation AS entity_source_pk_value,
 		v.id_dataset,
 		v.id_nomenclature_geo_object_nature,
 		v.id_nomenclature_grp_typ,
