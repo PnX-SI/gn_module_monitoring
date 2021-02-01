5f82edfaa3fca760a0ea45431633c8b9d277473b
joel.clement <joelclems@gmail.com>
2021-02-01 13:54:30 +0100

    date display chrome ref #72

--- frontend/app/utils/utils.ts
+++ frontend/app/utils/utils.ts
@@ -24,7 +24,7 @@ export class Utils {
   }
 
   static formatDate(val) {
-    return val ? new Date(val).toLocaleString('fr-FR', { timeZone: 'UTC' }).split(' ')[0] : val;
+    return val ? new Date(val).toLocaleString('fr-FR', { timeZone: 'UTC' }).replace(',', '').split(' ')[0] : val;
   }
 
   static mapDictToArray(dictIn: Object, processFunc= null, fieldName: string = null): Array<any> {
