a589f232fdc0624eddb035c29375e735c1fdeb6e
joel.clement <joelclems@gmail.com>
2021-07-13 14:03:57 +0200

    fix assetsDirectory ref #102

--- frontend/app/components/modules/modules.component.ts
+++ frontend/app/components/modules/modules.component.ts
@@ -19,6 +19,7 @@ export class ModulesComponent implements OnInit {
   frontendModuleMonitoringUrl: string;
   urlApplication: string;
   moduleMonitoringCode: string;
+  assetsDirectory: string;
 
   bLoading = false;
 
