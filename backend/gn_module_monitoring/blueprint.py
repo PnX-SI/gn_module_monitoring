466a6e98e0775d24c673aef8d648692ce55c8614
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2022-12-09 17:06:45 +0100

    Merged feat/bib-categorie-site
    Squashed commit of the following:
    
    commit 2bdc22a427d9c4b74849c3828b3be6d482c03e09
    Author: Maxime Vergez <maxime_vergez@natural-solutions.eu>
    Date:   Fri Dec 9 14:59:31 2022 +0100
    
        chore: changed json_schema into config
    
    commit 42793894ab9459fdb6e5b5a59e26250c1085b29d
    Author: Maxime Vergez <maxime_vergez@natural-solutions.eu>
    Date:   Tue Dec 6 18:37:41 2022 +0100
    
        feat(db): add foreign key from site to cat
    
        From t_base_sites to bib_categorie_site
    
    commit 5cb5dfa34bfc11b7eff1864867240d318cc6ad9f
    Author: Maxime Vergez <maxime_vergez@natural-solutions.eu>
    Date:   Tue Dec 6 16:47:27 2022 +0100
    
        feat(api): add admin interface
    
    commit 559a0ec35cbe2beed58fbd017df3c993735aa585
    Author: Maxime Vergez <maxime_vergez@natural-solutions.eu>
    Date:   Tue Dec 6 16:46:27 2022 +0100
    
        feat(db): add bib_categorie_site table
    
        Add migration and model

--- backend/gn_module_monitoring/blueprint.py
+++ backend/gn_module_monitoring/blueprint.py
@@ -4,6 +4,10 @@
 """
 
 from flask import Blueprint, current_app
+from geonature.core.admin.admin import admin as flask_admin
+from geonature.utils.env import DB
+
+from gn_module_monitoring.monitoring.admin import BibCategorieSiteView
 from .command.cmd import commands
 
 blueprint = Blueprint("monitorings", __name__)
@@ -12,3 +16,5 @@ from .routes import *  # noqa
 blueprint.cli.short_help = "Commandes pour l" "administration du module MONITORINGS"
 for cmd in commands:
     blueprint.cli.add_command(cmd)
+
+flask_admin.add_view(BibCategorieSiteView(DB.session, name="Catégories de sites", category="Monitorings"))
