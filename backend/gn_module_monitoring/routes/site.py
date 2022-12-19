322d7fc1f8683333ef1c5c35b2a300f731d8b23d
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2022-12-19 15:19:57 +0100

    Feat/monitoring sites (#16)
    * feat(api): wip began add site routes + tests
    
    With site categories
    Also add tests
    
    * feat(api): add more routes
    
    * test(api): add tests and fixtures
    
    * style(api): applied black
    
    * feat(db): add migration to remove id_module
    
    Column in t_sites_groups
    
    * refactor(api): move utils for routes from sites
    
    * feat(api): wip: add sites groups route
    
    * test(api): wip: begin adding fixture site_groups
    
    * fix: remove id_module in all models
    
    * chore: rename route for better consistency
    
    * tests: moved site_groups in tests and add tests
    
    * chore(api): applied black
    
    * refactor(api): add filter params function
    
    And refact routes to use it

---
+++ backend/gn_module_monitoring/routes/site.py
@@ -0,0 +1,47 @@
+from typing import Tuple
+
+from flask import request
+from flask.json import jsonify
+from geonature.core.gn_monitoring.models import TBaseSites
+from werkzeug.datastructures import MultiDict
+
+from gn_module_monitoring.blueprint import blueprint
+from gn_module_monitoring.monitoring.models import BibCategorieSite
+from gn_module_monitoring.utils.routes import (filter_params, get_limit_offset,
+                                               paginate)
+
+
+@blueprint.route("/sites/categories", methods=["GET"])
+def get_categories():
+    params = MultiDict(request.args)
+    limit, page = get_limit_offset(params=params)
+
+    query = filter_params(query=BibCategorieSite.query, params=params)
+    query = query.order_by(BibCategorieSite.id_categorie)
+    return paginate(query=query, object_name="categories", limit=limit, page=page)
+
+
+@blueprint.route("/sites/categories/<int:id_categorie>", methods=["GET"])
+def get_categories_by_id(id_categorie):
+    query = BibCategorieSite.query.filter_by(id_categorie=id_categorie)
+    res = query.first()
+
+    return jsonify(res.as_dict())
+
+
+@blueprint.route("/sites", methods=["GET"])
+def get_sites():
+    params = MultiDict(request.args)
+    # TODO: add filter support
+    limit, page = get_limit_offset(params=params)
+    query = TBaseSites.query.join(
+        BibCategorieSite, TBaseSites.id_categorie == BibCategorieSite.id_categorie
+    )
+    query = filter_params(query=query, params=params)
+    return paginate(query=query, object_name="sites", limit=limit, page=page)
+
+
+@blueprint.route("/sites/module/<string:module_code>", methods=["GET"])
+def get_module_sites(module_code: str):
+    # TODO: load with site_categories.json API
+    return jsonify({"module_code": module_code})
---
+++ backend/gn_module_monitoring/routes/sites_groups.py
@@ -0,0 +1,18 @@
+from flask import request
+from werkzeug.datastructures import MultiDict
+
+from gn_module_monitoring.blueprint import blueprint
+from gn_module_monitoring.monitoring.models import TMonitoringSitesGroups
+from gn_module_monitoring.utils.routes import (filter_params, get_limit_offset,
+                                               paginate)
+
+
+@blueprint.route("/sites_groups", methods=["GET"])
+def get_sites_groups():
+    params = MultiDict(request.args)
+    limit, page = get_limit_offset(params=params)
+
+    query = filter_params(query=TMonitoringSitesGroups.query, params=params)
+
+    query = query.order_by(TMonitoringSitesGroups.id_sites_group)
+    return paginate(query=query, object_name="sites_groups", limit=limit, page=page)
