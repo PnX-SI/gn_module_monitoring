e380da1ac09a4a647af1333e5d2595b71d147694
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2022-12-19 15:19:57 +0100

    Feat/monitoring sites (#16)
    * feat(api): wip began add site routes + tests
    
    With site categories
    Also add tests
    
    * feat(api): add more routes
    
    * test(api): add tests and fixtures
    
    * style(api): applied black
    
    * feat(db): add migration to remove id_module
    
    Column in t_sites_groups
    
    * refactor(api): move utils for routes from sites
    
    * feat(api): wip: add sites groups route
    
    * test(api): wip: begin adding fixture site_groups
    
    * fix: remove id_module in all models
    
    * chore: rename route for better consistency
    
    * tests: moved site_groups in tests and add tests
    
    * chore(api): applied black
    
    * refactor(api): add filter params function
    
    And refact routes to use it

---
+++ backend/gn_module_monitoring/migrations/f24adb481f54_remove_id_module_from_sites_groups.py
@@ -0,0 +1,54 @@
+"""remove_id_module_from_sites_groups
+
+Revision ID: f24adb481f54
+Revises: 
+Create Date: 2022-12-13 16:00:00.512562
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+from gn_module_monitoring import MODULE_CODE
+
+# revision identifiers, used by Alembic.
+revision = "f24adb481f54"
+down_revision = "b53bafb13ce8"
+branch_labels = None
+depends_on = None
+
+monitorings_schema = "gn_monitoring"
+
+
+def upgrade():
+    op.drop_column("t_sites_groups", "id_module", schema=monitorings_schema)
+
+
+def downgrade():
+    op.add_column(
+        "t_sites_groups",
+        sa.Column(
+            "id_module",
+            sa.Integer(),
+            sa.ForeignKey(
+                f"gn_commons.t_modules.id_module",
+                name="fk_t_sites_groups_id_module",
+                ondelete="CASCADE",
+                onupdate="CASCADE",
+            ),
+            nullable=True,
+        ),
+        schema=monitorings_schema,
+    )
+    # Cannot use orm here because need the model to be "downgraded" as well
+    # Need to set nullable True above for existing rows
+    # FIXME: find a better way because need to assign a module...
+    statement = sa.text(
+        f"""
+         update {monitorings_schema}.t_sites_groups
+         set id_module = (select id_module 
+                          from gn_commons.t_modules tm 
+                          where module_code = '\:module_code');
+        """
+    )
+    op.execute(statement, module_code=MODULE_CODE)
+    op.alter_column("t_sites_groups", "id_module", nullable=False)
