e345313bdf2cb1aff733bbf99374fc28a3da1248
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2022-12-19 15:19:57 +0100

    Feat/monitoring sites (#16)
    * feat(api): wip began add site routes + tests
    
    With site categories
    Also add tests
    
    * feat(api): add more routes
    
    * test(api): add tests and fixtures
    
    * style(api): applied black
    
    * feat(db): add migration to remove id_module
    
    Column in t_sites_groups
    
    * refactor(api): move utils for routes from sites
    
    * feat(api): wip: add sites groups route
    
    * test(api): wip: begin adding fixture site_groups
    
    * fix: remove id_module in all models
    
    * chore: rename route for better consistency
    
    * tests: moved site_groups in tests and add tests
    
    * chore(api): applied black
    
    * refactor(api): add filter params function
    
    And refact routes to use it

---
+++ backend/gn_module_monitoring/tests/fixtures/site.py
@@ -0,0 +1,45 @@
+import pytest
+from geoalchemy2.shape import from_shape
+from geonature.core.gn_monitoring.models import TBaseSites
+from geonature.utils.env import db
+from pypnnomenclature.models import BibNomenclaturesTypes, TNomenclatures
+from shapely.geometry import Point
+
+from gn_module_monitoring.monitoring.models import BibCategorieSite
+
+
+@pytest.fixture()
+def categories():
+    categories = [{"label": "gite", "config": {}}, {"label": "eolienne", "config": {}}]
+
+    categories = {cat["label"]: BibCategorieSite(**cat) for cat in categories}
+
+    with db.session.begin_nested():
+        db.session.add_all(categories.values())
+
+    return categories
+
+
+@pytest.fixture()
+def sites(users, categories):
+    user = users["user"]
+    geom_4326 = from_shape(Point(43, 24), srid=4326)
+    sites = {}
+    # TODO: get_nomenclature from label
+    site_type = TNomenclatures.query.filter(
+        BibNomenclaturesTypes.mnemonique == "TYPE_SITE", TNomenclatures.mnemonique == "Grotte"
+    ).one()
+    for i, key in enumerate(categories.keys()):
+        sites[key] = TBaseSites(
+            id_inventor=user.id_role,
+            id_digitiser=user.id_role,
+            base_site_name=f"Site{i}",
+            base_site_description=f"Description{i}",
+            base_site_code=f"Code{i}",
+            geom=geom_4326,
+            id_nomenclature_type_site=site_type.id_nomenclature,
+            id_categorie=categories[key].id_categorie,
+        )
+    with db.session.begin_nested():
+        db.session.add_all(sites.values())
+    return sites
