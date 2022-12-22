b55b38058a286fa49e0bb5e81067bb6d7999c0ff
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2022-12-22 14:27:07 +0100

    Feat/edit categories module (#19)
    * tests: make tests and fixtures work for modules
    
    By changing the way a monitoring module is created in the fixture
    Add two tests to check the relationship
    
    * feat(api): add categories in edit module
    
    * style: applied black

--- backend/gn_module_monitoring/tests/test_monitoring/test_models/test_module.py
+++ backend/gn_module_monitoring/tests/test_monitoring/test_models/test_module.py
@@ -1,11 +1,21 @@
 import pytest
+from geonature.utils.env import db
 
+from gn_module_monitoring.monitoring.models import TMonitoringModules
 from gn_module_monitoring.tests.fixtures.module import monitoring_module
-from gn_module_monitoring.tests.fixtures.site import categories
+from gn_module_monitoring.tests.fixtures.site import categories, site_type
 
 
 @pytest.mark.usefixtures("temporary_transaction")
 class TestModule:
-    def test_module(self, monitoring_module):
-        cateogories = monitoring_module.categories
-        assert False
+    def test_module(self, monitoring_module, categories):
+        cats = monitoring_module.categories
+        assert cats == list(categories.values())
+
+    def test_remove_categorie_from_module(self, monitoring_module, categories):
+        with db.session.begin_nested():
+            monitoring_module.categories.pop(0)
+
+        mon = TMonitoringModules.query.filter_by(id_module=monitoring_module.id_module).one()
+
+        assert len(mon.categories) == len(categories) - 1
