20648804c09328ec0221e7a3bb020911e2762abf
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2023-01-04 11:37:39 +0100

    Feat/create marshmallow schemas and remove id_module (#21)
    * feat(api): remove id_module from sites_complements
    
    Co-authored-by: andriacap <andriacap@users.noreply.github.com>
    
    * feat(api): create schema for sites_groups
    
    * test: add sites_group to site fixture
    
    * test: wip add test for sites_groups schemas
    
    * chore(api): remove depth parameter from paginate
    
    * test: updated to work with sites_group schema
    
    * feat: categorie site with marshmallow
    
    Test and marshmallow create/refactor to
    adapt for bibcategorie site paginate
    
    WIP : Adapt load_only site_type in test to "assert"
    same object when initiate BibCategorieSite
    
    [Refs ticket]: #3
    
    * feat(api): route /sites/categories/id with schema
    
    Changing the route to return a dump Marshmallow schema
    BibCategorieSitesSchema
    
    Reviewed-by: andriac
    [Refs ticket]: #3
    
    * test(api): routes get categoires label
    
    Change the "as_dict" by schema.dump in order
    to use the Marshmallow schema created
    
    Reviewed-by: andriac
    [Refs ticket]: #3
    
    * feat(api): Sites: cols to geoserializable + schema
    
    * style(api): applied black
    
    * test(api): add test for Site Schema
    
    * style(api): applied black to test_site
    
    * refactor(api): instantiate schema once
    
    Instead of for each all() iteration
    
    * chore(api): remove useless comments
    
    * chore(api): remove useless comments and imports
    
    Co-authored-by: andriacap <andriacap@users.noreply.github.com>
    Co-authored-by: Andria Capai <andria_capai@natural-solutions.eu>

--- backend/gn_module_monitoring/monitoring/models.py
+++ backend/gn_module_monitoring/monitoring/models.py
@@ -155,7 +155,7 @@ class TMonitoringVisits(TBaseVisits):
     )
 
 
-@geoserializable
+@geoserializable(geoCol="geom", idCol="id_base_site")
 class TMonitoringSites(TBaseSites):
     __tablename__ = "t_site_complements"
     __table_args__ = {"schema": "gn_monitoring"}
@@ -167,10 +167,6 @@ class TMonitoringSites(TBaseSites):
         DB.ForeignKey("gn_monitoring.t_base_sites.id_base_site"), nullable=False, primary_key=True
     )
 
-    id_module = DB.Column(
-        DB.ForeignKey("gn_commons.t_modules.id_module"),
-        nullable=False,
-    )
 
     id_sites_group = DB.Column(
         DB.ForeignKey(
--- backend/gn_module_monitoring/utils/routes.py
+++ backend/gn_module_monitoring/utils/routes.py
@@ -2,22 +2,23 @@ from typing import Tuple
 
 from flask import Response
 from flask.json import jsonify
+from marshmallow import Schema
 from sqlalchemy.orm import Query
 from werkzeug.datastructures import MultiDict
+
+from gn_module_monitoring.monitoring.schemas import paginate_schema
 
 
 def get_limit_offset(params: MultiDict) -> Tuple[int]:
     return params.pop("limit", 50), params.pop("offset", 1)
 
 
-def paginate(query: Query, object_name: str, limit: int, page: int, depth: int = 0) -> Response:
+def paginate(query: Query, schema: Schema, limit: int, page: int) -> Response:
     result = query.paginate(page=page, error_out=False, max_per_page=limit)
-    data = {
-        object_name: [res.as_dict(depth=depth) for res in result.items],
-        "count": result.total,
-        "limit": limit,
-        "offset": page - 1,
-    }
+    pagination_schema = paginate_schema(schema)
+    data = pagination_schema().dump(
+        dict(items=result.items, count=result.total, limit=limit, offset=page - 1)
+    )
     return jsonify(data)
 
 
