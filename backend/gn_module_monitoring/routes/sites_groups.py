0b25ef820ace83f08eccaaafcbd3d13bf2b8d7f5
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2023-01-04 12:39:04 +0100

    Feat/improve filter (#25)
    * feat(api): add Query Class to sites, grps & cats
    
    * feat(api): add sort and fix _get_model
    
    Via _get_entity
    
    * test(api): test sort query
    
    * feat(api): add api sort/sort_dir params
    
    To be able to sort through REST Api
    
    * fix(api): check if integer to avoid using ilike
    
    In filter_by_params
    
    * test(api): add test to check filter integer

--- backend/gn_module_monitoring/routes/sites_groups.py
+++ backend/gn_module_monitoring/routes/sites_groups.py
@@ -3,18 +3,26 @@ from werkzeug.datastructures import MultiDict
 
 from gn_module_monitoring.blueprint import blueprint
 from gn_module_monitoring.monitoring.models import TMonitoringSitesGroups
+from gn_module_monitoring.utils.routes import (
+    filter_params,
+    get_limit_offset,
+    get_sort,
+    paginate,
+    sort,
+)
 from gn_module_monitoring.monitoring.schemas import MonitoringSitesGroupsSchema
-from gn_module_monitoring.utils.routes import filter_params, get_limit_offset, paginate
 
 
 @blueprint.route("/sites_groups", methods=["GET"])
 def get_sites_groups():
     params = MultiDict(request.args)
     limit, page = get_limit_offset(params=params)
-
+    sort_label, sort_dir = get_sort(
+        params=params, default_sort="id_sites_group", default_direction="desc"
+    )
     query = filter_params(query=TMonitoringSitesGroups.query, params=params)
 
-    query = query.order_by(TMonitoringSitesGroups.id_sites_group)
+    query = sort(query=query, sort=sort_label, sort_dir=sort_dir)
     return paginate(
         query=query,
         schema=MonitoringSitesGroupsSchema,
--- backend/gn_module_monitoring/utils/routes.py
+++ backend/gn_module_monitoring/utils/routes.py
@@ -6,11 +6,16 @@ from marshmallow import Schema
 from sqlalchemy.orm import Query
 from werkzeug.datastructures import MultiDict
 
+from gn_module_monitoring.monitoring.queries import Query as MonitoringQuery
 from gn_module_monitoring.monitoring.schemas import paginate_schema
 
 
 def get_limit_offset(params: MultiDict) -> Tuple[int]:
     return int(params.pop("limit", 50)), int(params.pop("offset", 1))
+
+
+def get_sort(params: MultiDict, default_sort: str, default_direction) -> Tuple[str]:
+    return params.pop("sort", default_sort), params.pop("sort_dir", default_direction)
 
 
 def paginate(query: Query, schema: Schema, limit: int, page: int) -> Response:
@@ -22,7 +27,13 @@ def paginate(query: Query, schema: Schema, limit: int, page: int) -> Response:
     return jsonify(data)
 
 
-def filter_params(query: Query, params: MultiDict) -> Query:
+def filter_params(query: MonitoringQuery, params: MultiDict) -> MonitoringQuery:
     if len(params) != 0:
-        query = query.filter_by(**params)
+        query = query.filter_by_params(params)
+    return query
+
+
+def sort(query: MonitoringQuery, sort: str, sort_dir: str) -> MonitoringQuery:
+    if sort_dir in ["desc", "asc"]:
+        query = query.sort(label=sort, direction=sort_dir)
     return query
