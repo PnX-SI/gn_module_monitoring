0ee1cee75bd68fc195bf50d96bdcb99822ae63ef
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2023-01-04 11:37:39 +0100

    Feat/create marshmallow schemas and remove id_module (#21)
    * feat(api): remove id_module from sites_complements
    
    Co-authored-by: andriacap <andriacap@users.noreply.github.com>
    
    * feat(api): create schema for sites_groups
    
    * test: add sites_group to site fixture
    
    * test: wip add test for sites_groups schemas
    
    * chore(api): remove depth parameter from paginate
    
    * test: updated to work with sites_group schema
    
    * feat: categorie site with marshmallow
    
    Test and marshmallow create/refactor to
    adapt for bibcategorie site paginate
    
    WIP : Adapt load_only site_type in test to "assert"
    same object when initiate BibCategorieSite
    
    [Refs ticket]: #3
    
    * feat(api): route /sites/categories/id with schema
    
    Changing the route to return a dump Marshmallow schema
    BibCategorieSitesSchema
    
    Reviewed-by: andriac
    [Refs ticket]: #3
    
    * test(api): routes get categoires label
    
    Change the "as_dict" by schema.dump in order
    to use the Marshmallow schema created
    
    Reviewed-by: andriac
    [Refs ticket]: #3
    
    * feat(api): Sites: cols to geoserializable + schema
    
    * style(api): applied black
    
    * test(api): add test for Site Schema
    
    * style(api): applied black to test_site
    
    * refactor(api): instantiate schema once
    
    Instead of for each all() iteration
    
    * chore(api): remove useless comments
    
    * chore(api): remove useless comments and imports
    
    Co-authored-by: andriacap <andriacap@users.noreply.github.com>
    Co-authored-by: Andria Capai <andria_capai@natural-solutions.eu>

---
+++ backend/gn_module_monitoring/migrations/6673266fb79c_remove_id_module_from_sites_complements.py
@@ -0,0 +1,54 @@
+"""remove_id_module_from_sites_complements
+
+Revision ID: 6673266fb79c
+Revises: 
+Create Date: 2022-12-13 16:00:00.512562
+
+"""
+import sqlalchemy as sa
+from alembic import op
+
+from gn_module_monitoring import MODULE_CODE
+
+# revision identifiers, used by Alembic.
+revision = "6673266fb79c"
+down_revision = "e64bafb13ce8"
+branch_labels = None
+depends_on = None
+
+monitorings_schema = "gn_monitoring"
+
+
+def upgrade():
+    op.drop_column("t_site_complements", "id_module", schema=monitorings_schema)
+
+
+def downgrade():
+    op.add_column(
+        "t_site_complements",
+        sa.Column(
+            "id_module",
+            sa.Integer(),
+            sa.ForeignKey(
+                f"gn_commons.t_modules.id_module",
+                name="fk_t_site_complements_id_module",
+                ondelete="CASCADE",
+                onupdate="CASCADE",
+            ),
+            nullable=True,
+        ),
+        schema=monitorings_schema,
+    )
+    # Cannot use orm here because need the model to be "downgraded" as well
+    # Need to set nullable True above for existing rows
+    # FIXME: find a better way because need to assign a module...
+    statement = sa.text(
+        f"""
+         update {monitorings_schema}.t_site_complements
+         set id_module = (select id_module 
+                          from gn_commons.t_modules tm 
+                          where module_code = :module_code);
+        """
+    ).bindparams(module_code=MODULE_CODE)
+    op.execute(statement)
+    op.alter_column("t_site_complements", "id_module", nullable=False, schema=monitorings_schema)
