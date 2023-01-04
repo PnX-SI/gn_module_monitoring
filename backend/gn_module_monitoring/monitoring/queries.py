b964147b9fa001a52e22be3ecac882bfbef30e7b
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2023-01-04 12:39:04 +0100

    Feat/improve filter (#25)
    * feat(api): add Query Class to sites, grps & cats
    
    * feat(api): add sort and fix _get_model
    
    Via _get_entity
    
    * test(api): test sort query
    
    * feat(api): add api sort/sort_dir params
    
    To be able to sort through REST Api
    
    * fix(api): check if integer to avoid using ilike
    
    In filter_by_params
    
    * test(api): add test to check filter integer

---
+++ backend/gn_module_monitoring/monitoring/queries.py
@@ -0,0 +1,37 @@
+from flask_sqlalchemy import BaseQuery
+from sqlalchemy import Integer, and_
+from werkzeug.datastructures import MultiDict
+
+
+class Query(BaseQuery):
+    def _get_entity(self, entity):
+        if hasattr(entity, "_entities"):
+            return self._get_entity(entity._entities[0])
+        return entity.entities[0]
+
+    def _get_model(self):
+        # When sqlalchemy is updated:
+        # return self._raw_columns[0].entity_namespace
+        # But for now:
+        entity = self._get_entity(self)
+        return entity.c
+
+    def filter_by_params(self, params: MultiDict = None):
+        model = self._get_model()
+        and_list = []
+        for key, value in params.items():
+            column = getattr(model, key)
+            if isinstance(column.type, Integer):
+                and_list.append(column == value)
+            else:
+                and_list.append(column.ilike(f"%{value}%"))
+        and_query = and_(*and_list)
+        return self.filter(and_query)
+
+    def sort(self, label: str, direction: str):
+        model = self._get_model()
+        order_by = getattr(model, label)
+        if direction == "desc":
+            order_by = order_by.desc()
+
+        return self.order_by(order_by)
