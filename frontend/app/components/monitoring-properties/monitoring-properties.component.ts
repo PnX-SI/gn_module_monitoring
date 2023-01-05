951f4ef8eaf583bfc6b743f3079554c8930a312b
TheoLechemia <theo.lechemia@ecrins-parcnational.fr>
2023-01-05 13:52:42 +0100

    improve UI and replace dynamic form by pnx-dataset

--- frontend/app/components/monitoring-properties/monitoring-properties.component.ts
+++ frontend/app/components/monitoring-properties/monitoring-properties.component.ts
@@ -1,36 +1,37 @@
-import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
-import { MonitoringObject } from '../../class/monitoring-object';
-import { ConfigService } from '../../services/config.service';
-import { DataMonitoringObjectService } from '../../services/data-monitoring-object.service';
-import { CommonService } from '@geonature_common/service/common.service';
-import { MediaService } from '@geonature_common/service/media.service';
-import html2canvas from 'html2canvas';
+import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { MonitoringObject } from "../../class/monitoring-object";
+import { ConfigService } from "../../services/config.service";
+import { DataMonitoringObjectService } from "../../services/data-monitoring-object.service";
+import { CommonService } from "@geonature_common/service/common.service";
+import { MediaService } from "@geonature_common/service/media.service";
+import html2canvas from "html2canvas";
 import { MapService } from "@geonature_common/map/map.service";
 import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
-import { FormGroup, FormBuilder, Validators } from "@angular/forms";
-
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'pnx-monitoring-properties',
-  templateUrl: './monitoring-properties.component.html',
-  styleUrls: ['./monitoring-properties.component.css']
+  selector: "pnx-monitoring-properties",
+  templateUrl: "./monitoring-properties.component.html",
+  styleUrls: ["./monitoring-properties.component.css"],
 })
 export class MonitoringPropertiesComponent implements OnInit {
-
   @Input() obj: MonitoringObject;
   @Input() bEdit: boolean;
   @Output() bEditChange = new EventEmitter<boolean>();
 
   @Input() currentUser;
 
-
-  datasetForm: FormGroup;
-  datasetFormDef = [];
+  datasetForm = new FormControl();
   backendUrl: string;
   bUpdateSyntheseSpinner = false;
 
   public modalReference;
-  selectedDataSet = [];
+  selectedDataSet: Array<number> = [];
 
   constructor(
     private _configService: ConfigService,
@@ -38,33 +39,11 @@ export class MonitoringPropertiesComponent implements OnInit {
     private _dataService: DataMonitoringObjectService,
     private _commonService: CommonService,
     public ngbModal: NgbModal,
-    public mapservice: MapService,
-  ) { }
+    public mapservice: MapService
+  ) {}
 
   ngOnInit() {
     this.backendUrl = this._configService.backendUrl();
-    this.setDatasetFormDef();
-
-  }
-
-  setDatasetFormDef() {
-    this.datasetFormDef = [
-      {
-        "attribut_name": "id_dataset",
-        "type_widget": "datalist",
-        "attribut_label": "Choisir un jeu de données à télécharger",
-        "type_util": "dataset",
-        "api": "meta/datasets",
-        "application": "GeoNature",
-        "keyValue": "id_dataset",
-        "keyLabel": "dataset_shortname",
-        "params": {
-          "orderby": "dataset_name",
-          "module_code": this.obj.moduleCode
-        },
-        "required": true
-      },
-    ];
   }
 
   onEditClick() {
@@ -76,11 +55,17 @@ export class MonitoringPropertiesComponent implements OnInit {
     this._dataService.updateSynthese(this.obj.moduleCode).subscribe(
       () => {
         this.bUpdateSyntheseSpinner = false;
-        this._commonService.regularToaster('success', `La syntèse à été mise à jour pour le module ${this.obj.moduleCode}`);
+        this._commonService.regularToaster(
+          "success",
+          `La syntèse à été mise à jour pour le module ${this.obj.moduleCode}`
+        );
       },
       (err) => {
         this.bUpdateSyntheseSpinner = false;
-        this._commonService.regularToaster('error', `Erreur lors de la mise à jour de la syntèse pour le module ${this.obj.moduleCode} - ${err.error.message}`);
+        this._commonService.regularToaster(
+          "error",
+          `Erreur lors de la mise à jour de la syntèse pour le module ${this.obj.moduleCode} - ${err.error.message}`
+        );
       }
     );
   }
@@ -90,9 +75,9 @@ export class MonitoringPropertiesComponent implements OnInit {
     this.modalReference = this.ngbModal.open(modal, { size: "lg" });
   }
 
-  onDatasetChanged(event: any, i) {
+  onDatasetChanged(id_dataset: any, i) {
     //update the ui
-    this.selectedDataSet[i] = event.id_dataset;
+    this.selectedDataSet[i] = id_dataset;
   }
 
   getExportCsv(type, method, jd) {
@@ -101,31 +86,34 @@ export class MonitoringPropertiesComponent implements OnInit {
 
   //mje: generate PDF export
   processExportPdf(exportPdfConfig) {
-
     var map = this.mapservice.map;
     const $this = this;
 
     try {
-      var zoomInElement = document.querySelector('#monitoring-map-container .leaflet-control-zoom-in');
+      var zoomInElement = document.querySelector(
+        "#monitoring-map-container .leaflet-control-zoom-in"
+      );
       var snapshotElement = document.getElementById("geometry");
       var config = {
         allowTaint: true,
         useCORS: true,
         ignoreElements: function (element) {
-          return element.classList[0] == 'leaflet-control-zoom-in'
-            || element.classList[0] == 'leaflet-control-zoom-out'
-            || element.classList[0] == 'leaflet-control-layers-toggle'
-            || element.title == 'A JS library for interactive maps'
-            || element.placeholder == 'Rechercher un lieu'
+          return (
+            element.classList[0] == "leaflet-control-zoom-in" ||
+            element.classList[0] == "leaflet-control-zoom-out" ||
+            element.classList[0] == "leaflet-control-layers-toggle" ||
+            element.title == "A JS library for interactive maps" ||
+            element.placeholder == "Rechercher un lieu"
+          );
         },
-        logging: false
-      }
+        logging: false,
+      };
 
       html2canvas(snapshotElement, config).then(function (canvas) {
         var imgData = canvas.toDataURL("image/png");
         const extra_data = {
-          resolved_properties: $this.obj.resolvedProperties
-        }
+          resolved_properties: $this.obj.resolvedProperties,
+        };
         $this._dataService
           .postPdfExport(
             $this.obj.moduleCode,
@@ -134,23 +122,19 @@ export class MonitoringPropertiesComponent implements OnInit {
             exportPdfConfig.template,
             imgData,
             extra_data
-          ).subscribe(() => {
+          )
+          .subscribe(() => {
             $this._commonService.regularToaster(
               "success",
               "L'export pdf est prêt à être récupéré dans le dossier de Téléchargement"
             );
           });
       });
-
     } catch {
       $this._commonService.regularToaster(
         "error",
         "Une erreur est survenue durant l'export pdf"
       );
     }
-
   }
-
-
-
 }
