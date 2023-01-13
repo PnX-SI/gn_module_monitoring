6951fb569217764f3652620c2f4ea3108cc299f6
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2023-01-13 10:08:37 +0100

    Fix/pagination (#28)
    * fix(api): page - 1 that returned the wrong page
    
    * test(api): add test for paginate

--- backend/gn_module_monitoring/tests/test_utils/test_routes.py
+++ backend/gn_module_monitoring/tests/test_utils/test_routes.py
@@ -1,7 +1,9 @@
 import pytest
 from werkzeug.datastructures import MultiDict
 
-from gn_module_monitoring.utils.routes import get_limit_offset
+from gn_module_monitoring.monitoring.models import TMonitoringSites
+from gn_module_monitoring.monitoring.schemas import MonitoringSitesSchema
+from gn_module_monitoring.utils.routes import get_limit_offset, paginate
 
 
 @pytest.mark.parametrize("limit, offset", [("1", "2"), (1, 2), ("1", 2), (1, "2")])
@@ -12,3 +14,14 @@ def test_get_limit_offset(limit, offset):
 
     assert isinstance(comp_limit, int)
     assert isinstance(comp_offset, int)
+
+
+def test_paginate(sites):
+    limit = 1
+    page = 2
+
+    res = paginate(
+        query=TMonitoringSites.query, schema=MonitoringSitesSchema, limit=limit, page=page
+    )
+
+    assert res.json["offset"] == page
