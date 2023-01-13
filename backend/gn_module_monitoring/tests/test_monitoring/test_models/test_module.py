90c5e506003bd9d8cafb34d75710dbb6649fe428
Maxime Vergez <85738261+mvergez@users.noreply.github.com>
2023-01-13 09:32:29 +0100

    Fix/db migrations (#31)
    * feat(db): upgrade down_revision following rebase
    
    Since rebase with develop: changed the down_revision number
    
    * fix(db): fix bind params enabling downgrade
    
    Beforehand the downgrade was not possible...
    
    * refactor(db): removed cor_site_type_category
    
    * refactor(db): changed category into type in cor
    
    * refactor(db): create cor_type_site
    
    * fix(db): renamed column
    
    * refactor(api): update models to fit migrations
    
    * fix(db):change bib_categorie_site to bib_type_site
    
    Adding :
    cor_site_module
    cor_site_type
    revision alembic to create function and trigger in order to add
    bib_type_site but only with nomenclature 'TYPE_SITE'
    upgrade and downgrade works
    
    [Refs ticket]: #3
    Reviewed-by: andriac
    
    * fix(api): updated models from migrations
    
    * fix(api): wip: fix admin following migrations
    
    * fix(api): update routes and tests
    
    To match migration changes
    
    * feat: flask admin bib_type_site
    
    Change bib_categories to bib_type_site into flask admin
    Adding filtering in list label_fr of type_site to secure the unique
    constraint
    
    Reviewed-by: andriac
    [Refs ticket]: #3
    
    * fix(api): updated schema to match models
    
    * fix(api): module edition
    
    * style(api): uniformize type_site
    
    * style(api): change relationship name for type_site
    
    * feat(api): validator admin
    
    * fix(api): make unique BibTypeSite in admin
    
    * test(api): fix test when existing nomenclatures
    
    In database
    
    Co-authored-by: Andria Capai <andria_capai@natural-solutions.eu>

--- backend/gn_module_monitoring/tests/test_monitoring/test_models/test_module.py
+++ backend/gn_module_monitoring/tests/test_monitoring/test_models/test_module.py
@@ -6,14 +6,14 @@ from gn_module_monitoring.monitoring.models import TMonitoringModules
 
 @pytest.mark.usefixtures("temporary_transaction")
 class TestModule:
-    def test_module(self, monitoring_module, categories):
-        cats = monitoring_module.categories
-        assert cats == list(categories.values())
+    def test_module(self, monitoring_module, types_site):
+        types = monitoring_module.types_site
+        assert types == list(types_site.values())
 
-    def test_remove_categorie_from_module(self, monitoring_module, categories):
+    def test_remove_categorie_from_module(self, monitoring_module, types_site):
         with db.session.begin_nested():
-            monitoring_module.categories.pop(0)
+            monitoring_module.types_site.pop(0)
 
         mon = TMonitoringModules.query.filter_by(id_module=monitoring_module.id_module).one()
 
-        assert len(mon.categories) == len(categories) - 1
+        assert len(mon.types_site) == len(types_site) - 1
