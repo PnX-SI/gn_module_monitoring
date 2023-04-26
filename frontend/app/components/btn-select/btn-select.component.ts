f7f03c3fc6269db80f9f91f5a896b7edb166a4ff
andriacap <111564663+andriacap@users.noreply.github.com>
2023-04-26 10:20:31 +0200

    Feat/edit site (#56)
    * feat: edit site (init value btn-select)
    
    - Change button select to accept incoming intial values
    - Change visit component to add btn-select and form-g inside html
    
    - WIP: update form component when initial values is up to date
    
    Reviewed-by: andriac
    [Refs_ticket] :  #5 , #6
    
    * feat: get information when edit site
    
    - Get all fields from specific site.json into editform
    - Fix problem redirection if edit object site
    
    - WIP : check how to update specific fields from object
    - WIP : check how to manage listOption types site when reload or when
      come back into component after first init
    
    Reviewed-by: andriac
    [Refs_ticket]: #5 , #6
    
    * fix: forgot add apiService property binding
    
    Reviewed-by:andriac
    
    * feat: update site with good properties
    
    Change types_site object to array of ids
    Remove extra key "dataComplement" to assign config
    
    Reviewed-by:andriac
    [Refs_ticket]: #5 , #6
    
    * feat: prevent form appear if no type-site
    
    - Hide form if btn-list type-site not selected
    - Add custom error message mat-error if not selected (directive + custom
      message)
    
    WIP: error message is showing up only if not selected after touched .
    Maybe need to use asyncValidator ?
    
    Reviewed-by: andriac
    [Refs_tickets]: #5 , #6 , #54
    
    * feat: change order to emit event bEdit
    
    - Change order between change formService.changeData and bEditChange
    - Preprocess for types_site seems to be useless (see todo)
    
    Reviewed-by: andriac
    [Refs_ticket]: : #5 , #6 , #54
    
    * feat: solve request changes
    
    - Apply prettier
    - Change object key [''] by type properties
    - Remove unused preprocess_data lines
    
    [Refs_ticket] : closes #54

--- frontend/app/components/btn-select/btn-select.component.ts
+++ frontend/app/components/btn-select/btn-select.component.ts
@@ -8,12 +8,13 @@ import {
   Output,
   ViewChild,
 } from '@angular/core';
-import { FormControl } from '@angular/forms';
+import { FormControl, Validators } from '@angular/forms';
 import { MatAutocompleteSelectedEvent } from '@angular/material/autocomplete';
 import { Observable, iif, of } from 'rxjs';
 import { debounceTime, distinctUntilChanged, map, startWith, switchMap } from 'rxjs/operators';
 
 import { JsonData } from '../../types/jsondata';
+import { FormService } from '../../services/form.service';
 
 export interface EmptyObject {
   name: string;
@@ -27,8 +28,10 @@ export interface EmptyObject {
 export class BtnSelectComponent implements OnInit {
   selectable = true;
   removable = true;
+  isInit = false;
   separatorKeysCodes: number[] = [ENTER, COMMA];
   myControl = new FormControl();
+  listOpNeeded = new FormControl([],[Validators.required, Validators.minLength(1)])
   @Input() placeholderText: string = 'Selectionnez vos options dans la liste';
   @Input() titleBtn: string = 'Choix des options';
 
@@ -36,20 +39,30 @@ export class BtnSelectComponent implements OnInit {
   listOptionChosen: string[] = [];
   configObjAdded: JsonData = {};
   genericResponse: JsonData = {};
+  objToEdit: JsonData;
 
+  @Input() bEdit: boolean;
+  @Input() isInitialValues:boolean;
   @Input() paramToFilt: string;
   @Input() callBackFunction: (
     pageNumber: number,
     limit: number,
     valueToFilter: string
   ) => Observable<any>;
+  @Input() initValueFunction : ()=> JsonData;
   @ViewChild('optionInput') optionInput: ElementRef<HTMLInputElement>;
 
   @Output() public sendobject = new EventEmitter<JsonData>();
 
-  constructor() {}
+  constructor(private _formService: FormService) { }
 
   ngOnInit() {
+
+    if(this.isInitialValues && !this.isInit){
+      this.initFromExistingObj(this.paramToFilt)
+      this.objToEdit.map(val => this.addObject(val))
+      this.isInit = true
+    }
     this.filteredOptions = this.myControl.valueChanges.pipe(
       startWith(''),
       debounceTime(400),
@@ -63,6 +76,8 @@ export class BtnSelectComponent implements OnInit {
       }),
       map((res) => (res.length > 0 ? res : [{ name: 'Pas de résultats' }]))
     );
+    this.listOpNeeded.setValue(this.listOptionChosen)
+    this._formService.changeExtraFormControl(this.listOpNeeded,"listOptBtnSelect")
   }
 
   remove(option: string): void {
@@ -76,6 +91,8 @@ export class BtnSelectComponent implements OnInit {
       delete this.configObjAdded[option];
     }
     this.sendobject.emit(this.configObjAdded);
+    this.listOpNeeded.setValue(this.listOptionChosen)
+    this._formService.changeExtraFormControl(this.listOpNeeded,"listOptBtnSelect")
   }
 
   selected(event: MatAutocompleteSelectedEvent): void {
@@ -85,6 +102,8 @@ export class BtnSelectComponent implements OnInit {
       : null;
     this.optionInput.nativeElement.value = '';
     this.myControl.setValue(null);
+    this.listOpNeeded.setValue(this.listOptionChosen)
+    this._formService.changeExtraFormControl(this.listOpNeeded,"listOptBtnSelect")
   }
 
   filterOnRequest(val: string, keyToFilt: string): Observable<any> {
@@ -120,4 +139,16 @@ export class BtnSelectComponent implements OnInit {
     this.configObjAdded[name] = configAndId;
     this.sendobject.emit(this.configObjAdded);
   }
+
+  initFromExistingObj(keyToFilt: string){
+  const objInput = this.initValueFunction()
+  this.objToEdit = objInput .filter((obj) => {
+    Object.assign(obj, { name: obj[keyToFilt] })[keyToFilt];
+    delete obj[keyToFilt];
+    return obj;
+  })
+  this.objToEdit.map(obj => this.listOptionChosen.push(obj.name))
+
+  }
+
 }
