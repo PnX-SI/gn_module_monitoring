2e138a1607befd392df53841a11d1812424b6ab8
Andria Capai <andria_capai@natural-solutions.eu>
2023-05-15 18:39:59 +0200

    feat(front): make breadcrumn logic inside component
    - Get all information from loading component to build breadcrumb
      information
    - Fix call twice siteService (delete the one with snake_case)
    - Use logic from observable and snapshot url to satisfy all case to
      access to route (by component, directly by url etc)
    
    Reviewed-by: andriacap

--- frontend/app/services/object.service.ts
+++ frontend/app/services/object.service.ts
@@ -1,16 +1,17 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject,ReplaySubject } from "rxjs";
-import { endPoints } from "../enum/endpoints";
-import { ISitesGroup, ISiteType } from "../interfaces/geom";
+import { ReplaySubject } from "rxjs";
 import { IobjObs, ObjDataType } from "../interfaces/objObs";
 import { JsonData } from "../types/jsondata";
-
+import { IBreadCrumb } from "../interfaces/object";
 
 @Injectable()
 export class ObjectService {
   objObs: IobjObs<ObjDataType>;
-  private objSelected =  new ReplaySubject<any>(1);
+  private objSelected = new ReplaySubject<any>(1);
   currentObjSelected = this.objSelected.asObservable();
+
+  private parentObjSelected = new ReplaySubject<any>(1);
+  currentParentObjSelected = this.parentObjSelected.asObservable();
 
   private dataObjType = new ReplaySubject<IobjObs<JsonData>>(1);
   currentObjectType = this.dataObjType.asObservable();
@@ -18,36 +19,57 @@ export class ObjectService {
   private dataObjTypeParent = new ReplaySubject<IobjObs<JsonData>>(1);
   currentObjectTypeParent = this.dataObjTypeParent.asObservable();
 
+  private dataBreadCrumb = new ReplaySubject<IBreadCrumb[]>(1);
+  currentDataBreadCrumb = this.dataBreadCrumb.asObservable();
+
   constructor() {
     let storedObjectType = localStorage.getItem('storedObjectType');
     let storedObjectTypeParent = localStorage.getItem('storedObjectTypeParent');
-    let storedObjectSelected= localStorage.getItem('storedObjectSelected');
-    if (storedObjectType)
-        this.changeObjectType(JSON.parse(storedObjectType), false);
+    let storedObjectSelected = localStorage.getItem('storedObjectSelected');
+    let storedPArentObjectSelected = localStorage.getItem('storedParentObjectSelected');
+    let storedDataBreadCrumb = localStorage.getItem('storedDataBreadCrumb');
+    if (storedObjectType) this.changeObjectType(JSON.parse(storedObjectType), false);
 
     if (storedObjectTypeParent)
-        this.changeObjectTypeParent(JSON.parse(storedObjectTypeParent), false);
-        
-    if (storedObjectSelected)
-        this.changeSelectedObj(JSON.parse(storedObjectSelected), false);
-}
+      this.changeObjectTypeParent(JSON.parse(storedObjectTypeParent), false);
 
+    if (storedObjectSelected) this.changeSelectedObj(JSON.parse(storedObjectSelected), false);
 
-  changeObjectType(newObjType: IobjObs<JsonData>,storeObjectType: boolean = false) {
-    if (storeObjectType)
-      localStorage.setItem('storedObjectType', JSON.stringify(newObjType));
-     this.dataObjType.next(newObjType);
+    if (storedPArentObjectSelected)
+      this.changeSelectedObj(JSON.parse(storedPArentObjectSelected), false);
+
+    if (storedDataBreadCrumb) this.changeBreadCrumb(JSON.parse(storedDataBreadCrumb), false);
   }
 
-   changeObjectTypeParent(newObjType: IobjObs<JsonData>,storeObjectTypeParent: boolean = false) {
+  changeObjectType(newObjType: IobjObs<JsonData>, storeObjectType: boolean = false) {
+    if (storeObjectType) localStorage.setItem('storedObjectType', JSON.stringify(newObjType));
+    this.dataObjType.next(newObjType);
+  }
+
+  changeObjectTypeParent(newObjType: IobjObs<JsonData>, storeObjectTypeParent: boolean = false) {
     if (storeObjectTypeParent)
       localStorage.setItem('storedObjectTypeParent', JSON.stringify(newObjType));
-     this.dataObjTypeParent.next(newObjType);
+    this.dataObjTypeParent.next(newObjType);
   }
 
-  changeSelectedObj(newObjSelected:ObjDataType , storeObjectTypeSelected: boolean = false ){
+  changeSelectedObj(newObjSelected: ObjDataType | {}, storeObjectTypeSelected: boolean = false) {
     if (storeObjectTypeSelected)
       localStorage.setItem('storedObjectSelected', JSON.stringify(newObjSelected));
-     this.objSelected.next(newObjSelected);
+    this.objSelected.next(newObjSelected);
+  }
+
+  changeSelectedParentObj(
+    newObjParentSelected: ObjDataType | {},
+    storeParentObjectTypeSelected: boolean = false
+  ) {
+    if (storeParentObjectTypeSelected)
+      localStorage.setItem('storedParentObjectSelected', JSON.stringify(newObjParentSelected));
+    this.parentObjSelected.next(newObjParentSelected);
+  }
+
+  changeBreadCrumb(newDataBreadCrumb: IBreadCrumb[], storeDataBreadCrumb: boolean = false) {
+    if (storeDataBreadCrumb)
+      localStorage.setItem('storedDataBreadCrumb', JSON.stringify(newDataBreadCrumb));
+    this.dataBreadCrumb.next(newDataBreadCrumb);
   }
 }
