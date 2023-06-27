f795f402f9b42762cd21d03c6760d9be5a182708
Andria Capai <andria_capai@natural-solutions.eu>
2023-06-27 18:06:02 +0200

    fix: problem adding site child by data-table comp
    When trying to add siteGroup's site child data no pass between the form
    component and the component of sites-groups .
    Creation of resolver to load information of siteGroup selected in order
    to updateBreadCrumb but also to specify the siteGroup Parent.
    
    Reviewed-by: andriac

---
+++ frontend/app/resolver/create-site.resolver.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { SitesGroupService } from '../services/api-geom.service';
+import { Observable,of } from 'rxjs';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { ISitesGroup } from '../interfaces/geom';
+
+@Injectable({ providedIn: 'root' })
+export class CreateSiteResolver
+  implements Resolve<ISitesGroup|null>
+{
+  constructor(public service: SitesGroupService) {}
+
+  resolve(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<ISitesGroup|null> {
+    const siteGroupId = parseInt(route.paramMap.get('id'))
+    const $getSiteGroups = this.service.getById(siteGroupId).pipe((result)=>{return result})
+    return $getSiteGroups
+  }
+}
