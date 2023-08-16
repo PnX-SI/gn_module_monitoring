479fef9cd849c233b42aeeef59579d55ecc56ec4
Andria Capai <andria_capai@natural-solutions.eu>
2023-08-16 18:04:39 +0200

    feat: observers displaying and editing form
    Observers is now editable in form with the component pnx-observers
    
    Observers is displayed as "nom_complet" in components "properties" and
    "data-table"
    
    Review-by: andriacap

--- frontend/app/interfaces/geom.ts
+++ frontend/app/interfaces/geom.ts
@@ -41,6 +41,8 @@ export interface ISite extends IGeomObject {
   dataComplement: JsonData;
   types_site: JsonData[];
   id_sites_group: number;
+  id_inventor: string[];
+  inventor: string[];
 }
 
 export interface ISiteField extends Omit<ISite, 'types_site'> {
