e9b70d8947c5d50e2d50cbe05ce9834c39c9f345
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2023-12-08 15:21:35 +0100

    [frontend] Style margin and map height - cf #270

--- frontend/app/components/monitoring-map-list/monitoring-map-list.component.ts
+++ frontend/app/components/monitoring-map-list/monitoring-map-list.component.ts
@@ -6,7 +6,9 @@ import { FormService } from '../../services/form.service';
 import { ApiGeomService } from '../../services/api-geom.service';
 import { ConfigJsonService } from '../../services/config-json.service';
 import { ObjectService } from '../../services/object.service';
+import { CommonService } from '@geonature_common/service/common.service';
 import { FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'monitoring-map-list.component',
   templateUrl: './monitoring-map-list.component.html',
@@ -17,11 +19,26 @@ export class MonitoringMapListComponent {
   obj: any;
   bEdit: boolean;
   objForm: FormGroup;
+  heightMap: string="80vh";
   //
   displayMap: boolean = true;
   siteSiteGroup: SiteSiteGroup | null = null;
   apiService: ApiGeomService;
-  constructor(private _formService: FormService) {}
+
+  constructor(
+    private _formService: FormService,
+    private _commonService: CommonService
+  ) {}
+
+  ngAfterViewInit() {
+    const container = document.getElementById('object');
+    const height = this._commonService.calcCardContentHeight();
+    container.style.height = height - 40 + 'px';
+    setTimeout(() => {
+      this.heightMap = height - 80 + 'px';
+    });
+
+  }
 
   onActivate(component) {
     this._formService.currentFormMap
