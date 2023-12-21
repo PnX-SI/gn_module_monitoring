e90a8f03e4306a6f2af514a0f302f816141d3a3e
Maxime Vergez <maxime_vergez@natural-solutions.eu>
2023-12-21 19:09:25 +0100

    chore(api): fix sqlalchemy warning
    ViewOnly & scalar_subquery()

--- backend/gn_module_monitoring/monitoring/models.py
+++ backend/gn_module_monitoring/monitoring/models.py
@@ -305,6 +305,7 @@ class TMonitoringModules(TModules):
         primaryjoin=(corIndividualModule.c.id_module == id_module),
         secondaryjoin=(corIndividualModule.c.id_individual == TIndividuals.id_individual),
         foreign_keys=[corIndividualModule.c.id_individual, corIndividualModule.c.id_module],
+        viewonly=True
     )
     data = DB.Column(JSONB)
 
@@ -359,7 +360,7 @@ TIndividuals.nb_sites = column_property(
             TMarkingEvent.id_individual == TIndividuals.id_individual,
             TMarkingEvent.id_base_marking_site == TMonitoringSites.id_base_site,
         )
-    )
+    ).scalar_subquery()
 )
 
 TMonitoringSites.nb_individuals = column_property(
@@ -368,7 +369,7 @@ TMonitoringSites.nb_individuals = column_property(
             TMarkingEvent.id_base_marking_site == TMonitoringSites.id_base_site,
             TMarkingEvent.id_individual == TIndividuals.id_individual,
         )
-    )
+    ).scalar_subquery()
 )
 
 # note the alias is mandotory otherwise the where is done on the subquery table
