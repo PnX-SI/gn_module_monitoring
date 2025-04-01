ad4bb75116c6f3d0a3c4517b02cccf3f7aaed37b
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2025-04-01 11:22:26 +0200

    Fix : appel multiple (et croissant) à la route /module lors de changement de page

--- frontend/app/components/modules/modules.component.ts
+++ frontend/app/components/modules/modules.component.ts
@@ -1,12 +1,9 @@
-import { Utils } from './../../utils/utils';
 import { Component, OnInit } from '@angular/core';
-import { concatMap, map, mergeMap } from 'rxjs/operators';
+import { concatMap, map } from 'rxjs/operators';
 
 /** services */
 import { DataMonitoringObjectService } from '../../services/data-monitoring-object.service';
 import { ConfigService } from '../../services/config.service';
-import { get } from 'https';
-import { AuthService, User } from '@geonature/components/auth/auth.service';
 import { TPermission } from '../../types/permission';
 import { ObjectsPermissionMonitorings } from '../../enum/objectPermission';
 import { PermissionService } from '../../services/permission.service';
@@ -18,7 +15,6 @@ import { TOOLTIPMESSAGEALERT } from '../../constants/guard';
   styleUrls: ['./modules.component.css'],
 })
 export class ModulesComponent implements OnInit {
-  currentUser: User;
   canAccessSite: boolean = false;
   currentPermission: TPermission = {
     [ObjectsPermissionMonitorings.MONITORINGS_GRP_SITES]: {
@@ -39,10 +35,6 @@ export class ModulesComponent implements OnInit {
   titleModule: string;
   modules: Array<any> = [];
 
-  backendUrl: string;
-  frontendModuleMonitoringUrl: string;
-  urlApplication: string;
-  moduleMonitoringCode: string;
   assetsDirectory: string;
 
   bLoading = false;
@@ -50,7 +42,6 @@ export class ModulesComponent implements OnInit {
   toolTipNotAllowed: string = TOOLTIPMESSAGEALERT;
 
   constructor(
-    private _auth: AuthService,
     private _dataMonitoringObjectService: DataMonitoringObjectService,
     private _configService: ConfigService,
     private _permissionService: PermissionService
@@ -58,46 +49,30 @@ export class ModulesComponent implements OnInit {
 
   ngOnInit() {
     this.bLoading = true;
-    this._configService
-      .init()
-      .pipe(
-        concatMap(() =>
-          this._dataMonitoringObjectService.getCruvedMonitoring().pipe(
-            map((listObjectCruved: Object) => {
-              this._permissionService.setPermissionMonitorings(listObjectCruved);
-            })
-          )
-        ),
-        concatMap(() =>
-          this._permissionService.currentPermissionObj.pipe(
-            map((permissionObject: TPermission) => (this.currentPermission = permissionObject))
-          )
-        ),
-        concatMap(
-          this._dataMonitoringObjectService.getModules.bind(this._dataMonitoringObjectService)
-        )
-      )
-      .subscribe((modules: Array<any>) => {
-        this.modules = modules.filter((m) => m.cruved.R >= 1);
-        this.backendUrl = this._configService.backendUrl();
-        this.frontendModuleMonitoringUrl = this._configService.frontendModuleMonitoringUrl();
-        this.moduleMonitoringCode = this._configService.moduleMonitoringCode();
-        this.urlApplication = this._configService.urlApplication();
-        this.assetsDirectory = `${this._configService.backendUrl()}/${
-          this._configService.appConfig.MEDIA_URL
-        }/monitorings/`;
-        this.bLoading = false;
-        this.description = this._configService.descriptionModule();
-        this.titleModule = this._configService.titleModule();
 
+    // Paramètre d'affichage
+    this.assetsDirectory = `${this._configService.backendUrl()}/${
+      this._configService.appConfig.MEDIA_URL
+    }/monitorings/`;
+    this.description = this._configService.descriptionModule();
+    this.titleModule = this._configService.titleModule();
+
+    // Récupération des permissions et de la liste des modules
+    return this._dataMonitoringObjectService
+      .getCruvedMonitoring()
+      .pipe(
+        map((listObjectCruved: Object) => {
+          this._permissionService.setPermissionMonitorings(listObjectCruved);
+        }),
+        concatMap(() => this._dataMonitoringObjectService.getModules())
+      )
+      .subscribe((modules) => {
+        this.currentPermission = this._permissionService.getPermissionUser();
         this.canAccessSite =
           this.currentPermission.MONITORINGS_SITES.canRead ||
           this.currentPermission.MONITORINGS_GRP_SITES.canRead;
+        this.modules = modules.filter((m) => m.cruved.R >= 1);
+        this.bLoading = false;
       });
-
-    this.currentUser = this._auth.getCurrentUser();
-
-    this.currentUser['cruved'] = {};
-    this.currentUser['cruved_objects'] = {};
   }
 }
