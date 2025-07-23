dd21387f3c434a790d0d3f5c0350bed9490a6552
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2025-07-23 12:08:18 +0200

    Rebase with develop

--- backend/gn_module_monitoring/tests/fixtures/generic.py
+++ backend/gn_module_monitoring/tests/fixtures/generic.py
@@ -1,5 +1,5 @@
 from sqlalchemy import select
-
+import pytest
 from geonature.utils.env import db
 from geonature.core.gn_permissions.models import (
     PermAction,
@@ -20,6 +20,57 @@ from pypnusershub.db.models import (
 from gn_module_monitoring.monitoring.models import TMonitoringModules
 
 
+def add_user_permission(
+    module_code, user, scope, type_code_object, code_action="CRUVED", sensitivity_filter=None
+):
+    """
+    Add permissions to a user in a module.
+
+    Parameters
+    ----------
+    module_code : str
+        Code of the module to add the permission to.
+    user : User
+        User to add the permission to.
+    scope : int
+        Scope value for the permission.
+    type_code_object : str
+        Type of the object to add the permission to.
+    code_action : str, optional
+        Code of the action to add the permission for. Default is "CRUVED".
+
+    Notes
+    -----
+    The function will add the permission to the specified module and object with the given scope.
+    If the scope is 3, the scope_value will be set to None.
+    """
+    module = db.session.execute(
+        select(TModules).where(TModules.module_code == module_code)
+    ).scalar_one()
+    actions = {
+        code: db.session.execute(
+            select(PermAction).where(PermAction.code_action == code)
+        ).scalar_one()
+        for code in code_action
+    }
+    with db.session.begin_nested():
+        if scope > 0:
+            object_all = db.session.scalars(
+                select(PermObject).where(PermObject.code_object == type_code_object)
+            ).all()
+            for action in actions.values():
+                for obj in object_all + module.objects:
+                    permission = Permission(
+                        role=user,
+                        action=action,
+                        module=module,
+                        object=obj,
+                        scope_value=scope if scope != 3 else None,
+                        sensitivity_filter=sensitivity_filter,
+                    )
+                    db.session.add(permission)
+
+
 @pytest.fixture(scope="session")
 def create_user():
     def _create_user(
@@ -38,13 +89,6 @@ def create_user():
 
         if not modules:
             modules = db.session.scalars(select(TModules)).all()
-
-        actions = {
-            code: db.session.execute(
-                select(PermAction).where(PermAction.code_action == code)
-            ).scalar_one()
-            for code in "CRUVED"
-        }
 
         type_code_object = [
             "MONITORINGS_MODULES",
@@ -73,49 +117,26 @@ def create_user():
                 id_role=user.id_role, id_application=app.id_application, id_profil=profil.id_profil
             )
             db.session.add(right)
-            if scope > 0:
-                for co in type_code_object:
-                    object_all = db.session.scalars(
-                        select(PermObject).where(PermObject.code_object == co)
-                    ).all()
-                    for action in actions.values():
-                        for module in modules:
-                            for obj in object_all + module.objects:
-                                permission = Permission(
-                                    role=user,
-                                    action=action,
-                                    module=module,
-                                    object=obj,
-                                    scope_value=scope if scope != 3 else None,
-                                    sensitivity_filter=sensitivity_filter,
-                                )
-                                db.session.add(permission)
+
+        for module in modules:
+            for code_object in type_code_object:
+                add_user_permission(
+                    module.module_code,
+                    user,
+                    scope,
+                    code_object,
+                    code_action="CRUVED",
+                    sensitivity_filter=sensitivity_filter,
+                )
+
         return user
 
     return _create_user
 
 
-@pytest.fixture(scope="session")
-def monitorings_users(app, create_user):
-    organisme = Organisme(nom_organisme="Autre")
-    db.session.add(organisme)
-    users = {}
-    users_to_create = [
-        ("noright_user", organisme, 0),
-        ("stranger_user", None, 2),
-        ("associate_user", organisme, 2),
-        ("self_user", organisme, 1),
-        ("user", organisme, 2),
-        ("admin_user", organisme, 3),
-    ]
-    for username, *args in users_to_create:
-        users[username] = create_user(username, *args)
-    return users
-
-
 @pytest.fixture()
 def create_test_module_user(install_module_test, create_user):
-    """user with right to read MONITORINGS_SITES of the test module because she is the digitiser of the sites"""
+    """user with right to read MONITORINGS_SITES of the test module because he is the digitiser of the sites"""
 
     def _create_test_module_user():
         module = db.session.execute(
