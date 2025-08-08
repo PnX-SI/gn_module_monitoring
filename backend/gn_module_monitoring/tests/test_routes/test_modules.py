6333c4cc085e0c67c74f8fdc212672fe282dee24
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2025-08-08 12:16:19 +0200

    [backend] Add object_code for get_modules_api routes

--- backend/gn_module_monitoring/tests/test_routes/test_modules.py
+++ backend/gn_module_monitoring/tests/test_routes/test_modules.py
@@ -12,6 +12,13 @@ class TestModules:
 
     def test_get_modules_api(self, users):
         set_logged_user_cookie(self.client, users["admin_user"])
+        add_user_permission(
+            "MONITORINGS",
+            users["admin_user"],
+            scope=3,
+            type_code_object="ALL",
+            code_action="R",
+        )
         r = self.client.get(url_for("monitorings.get_modules_api"))
         # TODO test response
         assert r.status_code == 200
