fdcf585286c381d2c26e4beef788e915247dd3cd
Jacques Fize <4259846+jacquesfize@users.noreply.github.com>
2025-10-22 13:00:18 +0200

    Add a Config Resolver launched on every route (#515)

---
+++ frontend/app/resolver/config.resolver.ts
@@ -0,0 +1,21 @@
+// resolver.service.ts
+import { Injectable } from '@angular/core';
+import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
+import { Observable, of, EMPTY } from 'rxjs';
+import { ConfigServiceG } from '../services/config-g.service';
+@Injectable({
+  providedIn: 'root',
+})
+export class ModuleConfigResolver implements Resolve<any> {
+  constructor(private configServiceG: ConfigServiceG) {}
+
+  resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
+    const moduleCode = route.params?.moduleCode;
+    if (moduleCode) {
+      return this.configServiceG.init(moduleCode).pipe(() => {
+        return of({ moduleCode: route.params.moduleCode });
+      });
+    }
+    return EMPTY;
+  }
+}
--- frontend/app/services/config-g.service.ts
+++ frontend/app/services/config-g.service.ts
@@ -1,11 +1,13 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { ModuleService } from '@geonature/services/module.service';
-import { of , Observable} from 'rxjs';
-import { mergeMap , map} from 'rxjs/operators';
+import { of, Observable } from 'rxjs';
+import { mergeMap, map } from 'rxjs/operators';
 import { ConfigService as GnConfigService } from '@geonature/services/config.service';
 
-@Injectable()
+@Injectable({
+  providedIn: 'root', // a changer :/
+})
 export class ConfigServiceG {
   protected _config: any;
   protected _moduleCode: string | null = null;
@@ -13,7 +15,7 @@ export class ConfigServiceG {
   constructor(
     protected _http: HttpClient,
     protected _moduleService: ModuleService,
-    public appConfig: GnConfigService
+    public geonatureConfig: GnConfigService
   ) {}
 
   // Getter  de la config
@@ -27,38 +29,32 @@ export class ConfigServiceG {
     moduleCode = moduleCode || 'generic';
     if (this._moduleCode === moduleCode && this._config) {
       return of(true);
-    }  else {
+    } else {
       return this.loadConfig(moduleCode);
     }
   }
 
-  loadConfig(moduleCode: string) :Observable<boolean> {
-    console.log('ConfigServiceG loadConfig for moduleCode : ', moduleCode);
+  loadConfig(moduleCode: string): Observable<boolean> {
     const urlConfig =
       moduleCode === 'generic'
         ? `${this.backendModuleUrl()}/refacto/config`
         : `${this.backendModuleUrl()}/refacto/config/${moduleCode}`;
 
-
     return this._http.get<any>(urlConfig).pipe(
-      mergeMap(
-        (config: any  ) => {
-          this._config =  config
-          return of(true);
-        }
-      ))
-
+      mergeMap((config: any) => {
+        this._config = config;
+        return of(true);
+      })
+    );
   }
 
   /** Backend Module Url */
   backendModuleUrl() {
     // Test if api endpoint have a final slash
-    let api_url = this.appConfig.API_ENDPOINT;
+    let api_url = this.geonatureConfig.API_ENDPOINT;
     if (api_url.substring(api_url.length - 1, 1) !== '/') {
       api_url = api_url + '/';
     }
     return `${api_url}${this._moduleService.currentModule.module_path}`;
   }
-
-
 }
