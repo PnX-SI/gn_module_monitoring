5c7951c0347e0b32e837d0563c6c897bb79f7a9a
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2025-10-22 17:56:50 +0200

    fix config resolver

--- frontend/app/resolver/config.resolver.ts
+++ frontend/app/resolver/config.resolver.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot, RouterStateSnapshot } from '@angular/router';
 import { Observable, of, EMPTY } from 'rxjs';
 import { ConfigServiceG } from '../services/config-g.service';
+import { mergeMap } from 'rxjs/operators';
 @Injectable({
   providedIn: 'root',
 })
@@ -12,9 +13,11 @@ export class ModuleConfigResolver implements Resolve<any> {
   resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<any> {
     const moduleCode = route.params?.moduleCode;
     if (moduleCode) {
-      return this.configServiceG.init(moduleCode).pipe(() => {
-        return of({ moduleCode: route.params.moduleCode });
-      });
+      return this.configServiceG.init(moduleCode).pipe(
+        mergeMap((data) => {
+          return of({ moduleCode: route.params.moduleCode });
+        })
+    );
     }
     return EMPTY;
   }
