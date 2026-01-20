bd38a60c108559e60b90e3245eb108f57d5605a4
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2026-01-20 13:45:31 +0100

    Fix permission scope for visit in module context

--- backend/gn_module_monitoring/routes/visit.py
+++ backend/gn_module_monitoring/routes/visit.py
@@ -35,26 +35,25 @@ def get_visits(object_type, module_code=None):
     # Retrieves visits that do not depend on modules
     OBJECT_CODE = current_app.config["MONITORINGS"].get("PERMISSION_LEVEL", {})["visit"]
 
-    modules = get_objet_with_permission_boolean(modules_object, object_code=OBJECT_CODE)
-    ids_modules_allowed = [module["id_module"] for module in modules if module["cruved"]["R"]]
     query = select(TMonitoringVisits)
-    query = query.options(joinedload(TMonitoringVisits.module)).where(
-        TMonitoringVisits.id_module.in_(ids_modules_allowed)
-    )
     query = filter_params(TMonitoringVisits, query=query, params=params)
-    query = sort(model=TMonitoringVisits, query=query, sort=sort_label, sort_dir=sort_dir)
-    if module_code:
+    query = sort(model=TMonitoringVisits, query=query, sort=sort_label, sort_dir=sort_dir) 
+    if  module_code and module_code != "MONITORINGS":
         query = query.where(
             TMonitoringVisits.module.has(TMonitoringModules.module_code == module_code)
         )
-    query_allowed = query
-    for module in modules:
-        if module["id_module"] in ids_modules_allowed:
-            query_allowed = TMonitoringVisits.filter_by_readable(
-                query=query_allowed, module_code=module["module_code"], object_code=OBJECT_CODE
+    else: 
+        modules_allowed = get_objet_with_permission_boolean(modules_object, object_code=OBJECT_CODE)
+        ids_modules_allowed = [module["id_module"] for module in modules_allowed if module["cruved"]["R"]]
+        query = query.options(joinedload(TMonitoringVisits.module)).where(
+            TMonitoringVisits.id_module.in_(ids_modules_allowed)
+        )
+        for module in modules_allowed: 
+            query = TMonitoringVisits.filter_by_readable(
+                query=query, module_code=module["module_code"], object_code=OBJECT_CODE
             )
     return paginate_scope(
-        query=query_allowed,
+        query=query,
         schema=MonitoringVisitsSchema,
         limit=limit,
         page=page,
