29e7a3eec48dfd7cc4e5fb95e869db0ef567c7ec
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2026-01-28 14:26:09 +0100

    Fix conf_test missing import db, select, tlist

--- backend/gn_module_monitoring/tests/test_routes/test_config.py
+++ backend/gn_module_monitoring/tests/test_routes/test_config.py
@@ -2,6 +2,9 @@ import pytest
 from flask import url_for, current_app
 
 from pypnusershub.tests.utils import set_logged_user_cookie
+from geonature.tests.test_users_menu import tlist
+from geonature.utils.env import db
+from sqlalchemy import select
 
 from gn_module_monitoring.monitoring.models import TMonitoringModules
 
