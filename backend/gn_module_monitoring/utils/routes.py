a33ae3350d2a00a657957e7ebf2b2f23532d92e1
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2026-07-21 10:06:02 +0200

    fix : deal with circular import

--- backend/gn_module_monitoring/utils/routes.py
+++ backend/gn_module_monitoring/utils/routes.py
@@ -4,7 +4,6 @@ from flask import Response, g
 from flask.json import jsonify
 
 from typing import Tuple, Optional
-from gn_module_monitoring.monitoring.definitions import MonitoringModels_dict
 from gn_module_monitoring.utils.module import get_specific_properties
 from marshmallow import Schema
 from werkzeug.datastructures import MultiDict
@@ -284,6 +283,8 @@ def process_json_data_for_db_upsert(config, properties, object_type):
     :param properties: dict, dictionary of properties
     :return: dict, processed properties dictionary
     """
+    from gn_module_monitoring.monitoring.definitions import MonitoringModels_dict
+
     data = {}
     class_object_type = MonitoringModels_dict[object_type]
     specific_properties = get_specific_properties(class_object_type, config, object_type)
