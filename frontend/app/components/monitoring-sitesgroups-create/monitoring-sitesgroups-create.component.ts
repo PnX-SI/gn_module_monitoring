4ee4abc491218f4cec5e12eb9971a1b8501cbafe
amandine-sahl <amandine.sahl@cevennes-parcnational.fr>
2026-07-21 10:22:54 +0200

    Add Observable CurrentEditMode in formService

--- frontend/app/components/monitoring-sitesgroups-create/monitoring-sitesgroups-create.component.ts
+++ frontend/app/components/monitoring-sitesgroups-create/monitoring-sitesgroups-create.component.ts
@@ -3,16 +3,12 @@ import { FormBuilder, FormGroup } from '@angular/forms';
 import { AuthService, User } from '@geonature/components/auth/auth.service';
 
 import { ActivatedRoute } from '@angular/router';
-import { mergeMap } from 'rxjs/operators';
-import { endPoints } from '../../enum/endpoints';
 import { ISitesGroup } from '../../interfaces/geom';
 import { FormService } from '../../services/form.service';
 import { SitesGroupService } from '../../services/api-geom.service';
 import { ObjectService } from '../../services/object.service';
 import { GeoJSONService } from '../../services/geojson.service';
-import { MonitoringObject } from '../../class/monitoring-object';
-import { MonitoringObjectService } from '../../services/monitoring-object.service';
-import { ConfigService } from '../../services/config.service';
+import { ConfigServiceG } from '../../services/config-g.service';
 
 @Component({
   selector: 'monitoring-sitesgroups-create',
@@ -20,66 +16,39 @@ import { ConfigService } from '../../services/config.service';
   styleUrls: ['./monitoring-sitesgroups-create.component.css'],
 })
 export class MonitoringSitesGroupsCreateComponent implements OnInit {
-  siteGroup: ISitesGroup;
-  objForm: FormGroup;
-  urlRelative: string;
   currentUser: User;
 
-  obj: MonitoringObject;
-  bEdit: boolean = true;
+  public sitesGroup: ISitesGroup;
 
-  moduleCode: string;
+  public moduleConfig;
+  public form: FormGroup;
 
   constructor(
     private _auth: AuthService,
     private _formService: FormService,
     private _formBuilder: FormBuilder,
     private _objService: ObjectService,
-    public sitesGroupService: SitesGroupService,
+    public _sitesGroupService: SitesGroupService,
     public geojsonService: GeoJSONService,
-    private _monitoringObjServiceMonitoring: MonitoringObjectService,
-    private _configService: ConfigService,
+    private _configServiceG: ConfigServiceG,
     private _route: ActivatedRoute
   ) {}
 
   ngOnInit() {
-    this.moduleCode = this._route.snapshot.data.createSitesGroups.moduleCode;
+    // Initialisation des variables
+    this.moduleConfig = this._configServiceG.config();
+    this.form = this._formBuilder.group({});
+    this.currentUser = this._auth.getCurrentUser();
+    // Création d'un nouvel objet site group
+    this.sitesGroup = null;
 
     // breadcrumb
     const queryParams = this._route.snapshot.queryParams;
-    this._objService.loadBreadCrumb(this.moduleCode, 'site', null, queryParams);
+    const moduleCode = this._configServiceG.moduleCode();
+    this._objService.loadBreadCrumb(moduleCode, 'site', null, queryParams);
 
-    this.bEdit = true;
-    this.objForm = this._formBuilder.group({});
-
-    const elements = document.getElementsByClassName('monitoring-map-container');
-    if (elements.length >= 1) {
-      elements[0].remove();
-    }
-
-    this.obj = new MonitoringObject(
-      this.moduleCode,
-      'sites_group',
-      null,
-      this._monitoringObjServiceMonitoring
-    );
-    this.currentUser = this._auth.getCurrentUser();
-
-    this._route.paramMap
-      .pipe(
-        mergeMap(() => {
-          return this._configService.init(this.moduleCode);
-        }),
-        mergeMap(() => {
-          return this.obj.get(0);
-        })
-      )
-      .subscribe((params) => {
-        this.obj.initTemplate();
-        this._formService.changeCurrentEditMode(this.bEdit);
-
-        this.obj.bIsInitialized = true;
-      });
+    // Passage en mode édition
+    this._formService.changeCurrentEditMode(true);
   }
 
   ngOnDestroy() {
