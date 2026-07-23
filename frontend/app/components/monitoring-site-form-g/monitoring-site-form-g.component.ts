3c254bc7c8a5499b68fb478e993f21c90b5405c5
Pierre-Narcisi <pierre.narcisi@epitech.eu>
2026-07-23 17:00:19 +0200

    manage type site

--- frontend/app/components/monitoring-site-form-g/monitoring-site-form-g.component.ts
+++ frontend/app/components/monitoring-site-form-g/monitoring-site-form-g.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit, Input, AfterViewInit, Output, EventEmitter } from '@angular/core';
 import { FormGroup, FormBuilder, Validators, FormControl, FormArray } from '@angular/forms';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { TranslateService } from '@ngx-translate/core';
+import { Location } from '@angular/common';
 
-import { MonitoringFormGComponent } from '../monitoring-form-g/monitoring-form-g.component';
 import { CommonService } from '@geonature_common/service/common.service';
 import { DynamicFormService } from '@geonature_common/form/dynamic-form-generator/dynamic-form.service';
-import { Location } from '@angular/common';
+
+import { MonitoringFormGComponent } from '../monitoring-form-g/monitoring-form-g.component';
 import { FormService } from '../../services/form.service';
 import { DataUtilsService } from '../../services/data-utils.service';
 import { JsonData } from '../../types/jsondata';
@@ -18,6 +21,13 @@ import { NavigationService } from '../../services/navigation.service';
   styleUrls: ['./monitoring-site-form-g.component.css'],
 })
 export class MonitoringSiteFormGComponent extends MonitoringFormGComponent {
+  private allSiteFormsDefinition: JsonData = [];
+  public typeSiteFormsDefinition: { idType: number; name: string; fields: JsonData[] }[] = [];
+
+  private typeSiteFieldsCache: { [idType: number]: JsonData[] } = {};
+
+  private typesSiteValueChangesSub: Subscription;
+
   constructor(
     _commonService: CommonService,
     _formService: FormService,
@@ -27,7 +37,8 @@ export class MonitoringSiteFormGComponent extends MonitoringFormGComponent {
     _location: Location,
     _geojsonService: GeoJSONService,
     _navigationService: NavigationService,
-    _route: ActivatedRoute
+    _route: ActivatedRoute,
+    translate: TranslateService
   ) {
     super(
       _commonService,
@@ -38,7 +49,70 @@ export class MonitoringSiteFormGComponent extends MonitoringFormGComponent {
       _location,
       _geojsonService,
       _navigationService,
-      _route
+      _route,
+      translate
     );
+  }
+
+  initFormDefiniton(schema: JsonData, meta: JsonData) {
+    this.allSiteFormsDefinition = super.initFormDefiniton(schema, meta);
+    this.typeSiteFieldsCache = {};
+    return (this.allSiteFormsDefinition as any[]).filter(
+      (formDef) => !(formDef.id_types_site || []).length
+    );
+  }
+
+  private getTypeSiteFields(idType: number): JsonData[] {
+    if (!this.typeSiteFieldsCache[idType]) {
+      this.typeSiteFieldsCache[idType] = (this.allSiteFormsDefinition as any[]).filter((formDef) =>
+        (formDef.id_types_site || []).includes(idType)
+      );
+    }
+    return this.typeSiteFieldsCache[idType];
+  }
+
+  onDynamicFormGroupChange() {
+    super.onDynamicFormGroupChange();
+
+    this.typesSiteValueChangesSub?.unsubscribe();
+    const typesSiteControl = this.form.get('types_site');
+    if (!typesSiteControl) {
+      this.typeSiteFormsDefinition = [];
+      return;
+    }
+    this.updateTypeSiteFormsDefinition(typesSiteControl.value || []);
+    this.typesSiteValueChangesSub = typesSiteControl.valueChanges.subscribe(
+      (idTypesSite: number[]) => {
+        this.updateTypeSiteFormsDefinition(idTypesSite || []);
+      }
+    );
+  }
+
+  updateTypeSiteFormsDefinition(typesSiteids: number[]) {
+    const typesSiteConfig = this.config['types_site'] || {};
+    this.typeSiteFormsDefinition = typesSiteids
+      .filter((idType) => typesSiteConfig[idType])
+      .map((idType) => ({
+        idType,
+        name: typesSiteConfig[idType]['name'],
+        fields: this.getTypeSiteFields(idType),
+      }));
+  }
+
+  resetDynamicForm() {
+    super.resetDynamicForm();
+    this.typeSiteFormsDefinition = this.typeSiteFormsDefinition.map((typeSite) => ({
+      ...typeSite,
+      fields: typeSite.fields.map((formDef) => ({ ...formDef })),
+    }));
+  }
+
+  ngOnDestroy() {
+    this.typesSiteValueChangesSub?.unsubscribe();
+    super.ngOnDestroy();
+  }
+
+  trackByTypeSiteId(index: number, typeSite: any) {
+    return typeSite.idType;
   }
 }
