e002ea63126b97bc78b082ae18202f8b9d01376a
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2026-07-24 17:02:18 +0200

    chore: refact observation_detail route (remove module sort and call withour module code, add type annotation)

--- backend/gn_module_monitoring/routes/obs_detail.py
+++ backend/gn_module_monitoring/routes/obs_detail.py
@@ -7,14 +7,12 @@ from marshmallow import EXCLUDE
 from werkzeug.exceptions import Forbidden
 from gn_module_monitoring.command import permissions
 from sqlalchemy import select
-from sqlalchemy.orm import joinedload
 from werkzeug.datastructures import MultiDict
 
 from geonature.utils.env import db
 
 from gn_module_monitoring.blueprint import blueprint
 from geonature.core.gn_permissions import decorators as permissions
-from geonature.core.gn_permissions.tools import get_scope
 from gn_module_monitoring.monitoring.models import (
     TMonitoringModules,
     TMonitoringObservationDetails,
@@ -32,21 +30,19 @@ from gn_module_monitoring.utils.routes import (
     paginate_scope,
     process_json_data_for_db_upsert,
     sort,
-    get_objet_with_permission_boolean,
 )
-from gn_module_monitoring.routes.modules import get_modules
 
 default_route_object_type = "observation_detail"
 OBJECT_CODE = "MONITORINGS_VISITES"
 
 
 @blueprint.route(
-    "/refacto/obs_details/<int:_id>",
+    "/refacto/observation_details/<int:_id>",
     methods=["DELETE"],
     defaults={"object_type": default_route_object_type},
 )
 @permissions.check_cruved_scope("D", get_scope=True, object_code=OBJECT_CODE)
-def delete_obs_detail(scope, _id, object_type):
+def delete_obs_detail(scope, _id: int, object_type: str):
     obs_detail = db.get_or_404(TMonitoringObservationDetails, _id)
     if not obs_detail.has_instance_permission(scope=scope):
         raise Forbidden(
@@ -58,18 +54,18 @@ def delete_obs_detail(scope, _id, object_type):
 
 
 @blueprint.route(
-    "/<string:module_code>/obs_details",
+    "/<string:module_code>/observation_details",
     methods=["POST"],
     defaults={"object_type": default_route_object_type},
 )
 @permissions.check_cruved_scope("C", object_code=OBJECT_CODE)
-def post_obs_detail(object_type, module_code):
+def post_obs_detail(object_type: str, module_code: str):
     post_data = dict(request.get_json())
     return create_or_update_obs_detail(post_data, module_code=module_code)
 
 
 @blueprint.route(
-    "/<string:module_code>/obs_details/<int:_id>",
+    "/<string:module_code>/observation_details/<int:_id>",
     methods=["PATCH"],
     defaults={"object_type": default_route_object_type},
 )
@@ -87,14 +83,11 @@ def patch_obs_detail(scope, object_type: str, module_code: str, _id: int):
 
 
 @blueprint.route(
-    "/obs_details", methods=["GET"], defaults={"object_type": default_route_object_type}
-)
-@blueprint.route(
-    "/refacto/<string:module_code>/obs_details",
+    "/refacto/<string:module_code>/observation_details",
     methods=["GET"],
     defaults={"object_type": default_route_object_type},
 )
-def get_obs_details(object_type, module_code=None):
+def get_obs_details(object_type: str, module_code: str):
     object_code = "MONITORINGS_VISITES"
     params = MultiDict(request.args)
     limit, page = get_limit_page(params=params)
@@ -104,30 +97,15 @@ def get_obs_details(object_type, module_code=None):
     )
     query = select(TMonitoringObservationDetails)
 
-    if module_code:
-        query = query.where(
-            TMonitoringObservations.visit.has(
-                TMonitoringVisits.module.has(TMonitoringModules.module_code == module_code)
-            )
+    query = query.where(
+        TMonitoringObservations.visit.has(
+            TMonitoringVisits.module.has(TMonitoringModules.module_code == module_code)
         )
+    )
 
     query = filter_params(TMonitoringObservationDetails, query=query, params=params)
 
-    # PATCH order by modules
-    if sort_label == "modules":
-        query = (
-            query.join(TMonitoringObservationDetails.observation)
-            .join(TMonitoringObservations.visit)
-            .join(TMonitoringVisits.module)
-        )
-        module_order = TMonitoringModules.module_label
-        if sort_dir == "desc":
-            module_order = module_order.desc()
-        query = query.order_by(module_order)
-    else:
-        query = sort(
-            TMonitoringObservationDetails, query=query, sort=sort_label, sort_dir=sort_dir
-        )
+    query = sort(TMonitoringObservationDetails, query=query, sort=sort_label, sort_dir=sort_dir)
 
     query_allowed = TMonitoringObservationDetails.filter_by_readable(
         query=query,
@@ -142,13 +120,7 @@ def get_obs_details(object_type, module_code=None):
         params=params,
         specific_properties=specific_properties,
     )
-    print("query_allowed", query_allowed, "module_code", module_code)
-    if module_code:
-        schema = add_specific_attributes(
-            MonitoringObservationsDetailsSchema, object_type, module_code
-        )
-    else:
-        schema = MonitoringObservationsDetailsSchema
+    schema = add_specific_attributes(MonitoringObservationsDetailsSchema, object_type, module_code)
 
     return paginate_scope(
         query=query_allowed,
@@ -160,13 +132,12 @@ def get_obs_details(object_type, module_code=None):
 
 
 @blueprint.route(
-    "/obs_details/<string:module_code>/<int:id>",
+    "/observation_details/<string:module_code>/<int:id>",
     methods=["GET"],
     defaults={"object_type": default_route_object_type},
 )
 @permissions.check_cruved_scope("R", get_scope=True, object_code=OBJECT_CODE)
-def get_obs_detail_by_id(scope, module_code, id, object_type):
-    # print(scope, module_code, id, object_type)
+def get_obs_detail_by_id(scope: int, module_code: str, id: int, object_type: str):
     obs_detail = db.get_or_404(TMonitoringObservationDetails, id)
     if not obs_detail.has_instance_permission(scope=scope):
         raise Forbidden(
@@ -188,7 +159,6 @@ def create_or_update_obs_detail(post_data: dict, module_code: str = "generic"):
     :return: dict, serialized observation detail
     """
     config = get_config(module_code, force=True)
-    # print(config, "config", module_code, "module_code")
     process_data = process_json_data_for_db_upsert(config, post_data, default_route_object_type)
 
     obs_detail = MonitoringObservationsDetailsSchema(unknown=EXCLUDE).load(process_data)
