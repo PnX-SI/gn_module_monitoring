501f022c3b7ba11fd47025283be2f4c51fe25fe3
jacquesfize <jacques.fize@ecrins-parcnational.fr>
2026-07-28 13:11:16 +0200

    feat: add new components for individuals creation and details

--- frontend/app/components/monitoring-sitesgroups/monitoring-sitesgroups.component.ts
+++ frontend/app/components/monitoring-sitesgroups/monitoring-sitesgroups.component.ts
@@ -317,17 +317,10 @@ export class MonitoringSitesGroupsComponent extends MonitoringGeomComponent impl
     const queryParams = {
       parents_path: ['module'],
     };
-    if (type == 'individual') {
-      // Patch individual tant que la page détail des individus est générique
-      queryParams['parents_path'] = ['module', 'individual'];
-      this.router.navigate([`/monitorings/create_object/${this.moduleCode}/`, type], {
-        queryParams: queryParams,
-      });
-    } else {
-      this.router.navigate([`/monitorings/object/${this.moduleCode}/`, type, 'create'], {
-        queryParams: queryParams,
-      });
-    }
+
+    this.router.navigate([`/monitorings/object/${this.moduleCode}/`, type, 'create'], {
+      queryParams: queryParams,
+    });
   }
 
   onDelete(event) {
